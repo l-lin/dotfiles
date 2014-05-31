9dbb2e19d5e22add48cb59cf23f38f0690b42156
Louis LIN <louis.lin.87@gmail.com>
2014-05-31 17:35:51 +0200

    Move dotfiles + add bootstra.sh :tophat:

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Create/override dotfiles? (y/n) " -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --exclude ".git/" --exclude "bootstrap.sh" --exclude "README.md" --exclude "sublimetext/" -av -- . ~
+    echo
+    echo "Remember to execute the following command:"
+    echo "      source ~/.zshrc"
+fi
