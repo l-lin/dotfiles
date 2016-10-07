a5926477a90ea3eb662092fe50f6dc97a59f8228
Louis LIN <louis.lin.87@gmail.com>
2016-10-07 09:45:08 +0200

    Add VSCode

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ read -p "Create/override dotfiles? (y/n) " -n 1 -r
 echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --exclude ".git/" --exclude "bootstrap.sh" --exclude "README.md" --exclude "sublimetext/" --exclude "idea" -av -- . ~
+    rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --exclude ".git/" --exclude "bootstrap.sh" --exclude "README.md" --exclude "sublimetext/" --exclude "idea" --exclude "vscode" -av -- . ~
     echo
     echo "Remember to execute the following command:"
     echo "      source ~/.zshrc"
---
+++ vscode/README.md
@@ -0,0 +1,9 @@
+# Visual Studio Code
+
+## Extensions
+
+* One Monokai Theme
+* Git history
+* VSCode Gret Icons
+* Angular 2 TypeScript Snippets
+* File peek
