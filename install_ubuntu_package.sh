a7b3be856fb5d9bb4ccf42146dcefd46516a2f2d
Louis Lin <louis.lin.87@gmail.com>
2018-08-31 10:06:24 +0200

    feat(install): add jq

--- install_ubuntu_package.sh
+++ install_ubuntu_package.sh
@@ -5,7 +5,7 @@ set -x
 
 sudo apt-get update
 # Install additional commands
-sudo apt-get install build-essential cmake git zsh fortune cowsay colordiff vim exuberant-ctags tmux python-pip python-dev ruby-dev ruby htop xclip httpie silversearcher-ag nfs-common net-tools lm-sensors libgtop network-manager-openvpn openvpn network-manager-openvpn-gnome curl nnn
+sudo apt-get install build-essential cmake git zsh fortune cowsay colordiff vim exuberant-ctags tmux python-pip python-dev ruby-dev ruby htop xclip httpie silversearcher-ag nfs-common net-tools lm-sensors libgtop network-manager-openvpn openvpn network-manager-openvpn-gnome curl nnn jq
 echo "[-] Switch zsh as default shell"
 chsh -s $(which zsh)
 
