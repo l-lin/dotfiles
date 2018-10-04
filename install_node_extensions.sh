57ddaf5cada1355b7678b174dba4213660ef7df6
Louis Lin <lin.louis@protonmail.com>
2018-10-04 09:27:31 +0200

    feat(install): add tiny-care-terminal

--- install_node_extensions.sh
+++ install_node_extensions.sh
@@ -11,5 +11,7 @@ echo "[-] Installing tldr: http://tldr-pages.github.io/"
 npm i -g tldr
 echo "[-] Installing git-recall: https://github.com/Fakerr/git-recall"
 npm i -g git-recall
+echo "[-] Installing tiny-care-terminal: https://github.com/notwaldorf/tiny-care-terminal"
+npm i -g tiny-care-terminal
 
 exit 0
