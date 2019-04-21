cf39430926599a021f898d01153def16531e9815
Louis Lin <lin.louis@pm.me>
2019-04-21 14:32:59 +0200

    feat: use symlinks intead of copying

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-read -p "Create/override dotfiles? (y/n) " -n 1 -r
+set -e
+
+read -p "Create symlinks? (y/n) " -n 1 -r
 echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress \
-        --exclude "bootstrap.sh" \
-        --exclude "dotfiles.gif" \
-        --exclude ".git/" \
-        --exclude "idea" \
-        --exclude "README.md" \
-        --exclude "sublimetext/" \
-        --exclude "install.sh" \
-        -av -- . ~
+    readonly SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+    for file in symlinks/.*; do
+        basenameFile=$(basename "${file}")
+        [ -r "${file}" ] && [ -f "${file}" ] && rm -f ${HOME}/${basenameFile} && ln -s ${file} ${SYMLINK_PATH}/${basenameFile}
+    done
+
+    cp -R .config ~
+    cp -R .vim ~
+    
     echo
     echo "Remember to execute the following command:"
     echo "      source ~/.zshrc"
