e44f881be89c09debd743271ef9f7a33f10aa179
Louis Lin <lin.louis@pm.me>
2019-08-27 09:48:20 +0200

    bootstrap: handle symlinks in folder recursively

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,15 +6,22 @@ read -p "Create symlinks? (y/n) " -n 1 -r
 echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    readonly SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-    for file in symlinks/.*; do
-        basenameFile=$(basename "${file}")
-        [ -r "${file}" ] && [ -f "${file}" ] && rm -f ${HOME}/${basenameFile} && ln -s ${SCRIPT_DIR}/${file} ${HOME}/${basenameFile}
+    readonly script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+    symlinks_dir=${script_dir}/symlinks
+    cd ${symlinks_dir}
+    for file in $(find . -type f); do
+	echo "[-] processing ${file}"
+        basename_file=$(basename "${file}")
+    	home_file_dir=$(realpath $(dirname ${HOME}/${file}))
+    	dotfiles_file_dir=$(realpath $(dirname ${symlinks_dir}/${file}))
+        if [ ! -d ${home_file_dir} ]; then
+            mkdir -p ${home_file_dir}
+        fi
+        [ -r "${file}" ] && [ -f "${file}" ] && \
+            rm -f ${home_file_dir}/${basename_file} && \
+            ln -s ${dotfiles_file_dir}/${basename_file} ${home_file_dir}/${basename_file}
     done
 
-    cp -R .config ~
-    cp -R .vim ~
-    
     echo
     echo "Remember to execute the following command:"
     echo "      source ~/.zshrc"
