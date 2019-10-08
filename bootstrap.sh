c9454271af8a7cb115cdc9dc1eb21d116cbf1d2d
Louis Lin <lin.louis@pm.me>
2019-10-08 12:19:08 +0200

    zsh: add notes cmd

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,16 +10,16 @@ then
     symlinks_dir=${script_dir}/symlinks
     cd ${symlinks_dir}
     for file in $(find . -type f); do
-	echo "[-] processing ${file}"
-        basename_file=$(basename "${file}")
-    	home_file_dir=$(realpath $(dirname ${HOME}/${file}))
-    	dotfiles_file_dir=$(realpath $(dirname ${symlinks_dir}/${file}))
-        if [ ! -d ${home_file_dir} ]; then
-            mkdir -p ${home_file_dir}
-        fi
-        [ -r "${file}" ] && [ -f "${file}" ] && \
-            rm -f ${home_file_dir}/${basename_file} && \
-            ln -s ${dotfiles_file_dir}/${basename_file} ${home_file_dir}/${basename_file}
+      echo "[-] processing ${file}"
+      basename_file=$(basename "${file}")
+      home_file_dir=$(realpath $(dirname ${HOME}/${file}))
+      dotfiles_file_dir=$(realpath $(dirname ${symlinks_dir}/${file}))
+      if [ ! -d ${home_file_dir} ]; then
+          mkdir -p ${home_file_dir}
+      fi
+      [ -r "${file}" ] && [ -f "${file}" ] && \
+          rm -f ${home_file_dir}/${basename_file} && \
+          ln -s ${dotfiles_file_dir}/${basename_file} ${home_file_dir}/${basename_file}
     done
 
     echo
