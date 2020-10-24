7716525b0a184041137d7a3589e378c8395273e0
Louis Lin <lin.louis@pm.me>
2020-10-24 18:59:54 +0200

    zsh: add forgit

--- install_zsh_plugins.sh
+++ install_zsh_plugins.sh
@@ -8,5 +8,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}
 git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM}/plugins/git-open
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
 git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM}/plugins/zsh-nvm
+git clone https://github.com/wfxr/forgit ${ZSH_CUSTOM}/plugins/forgit
 git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM}/themes/spaceship-prompt
 ln -s ${ZSH_CUSTOM}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM}/themes/spaceship.zsh-theme
