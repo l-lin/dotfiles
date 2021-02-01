c94fc02301ad4db8536e7c6f84e9d4e3d76b237b
Louis Lin <lin.louis@pm.me>
2021-02-01 17:53:38 +0100

    install: add trelew

--- 02-install-misc.sh
+++ 02-install-misc.sh
@@ -120,8 +120,11 @@ sudo snap install hugo --channel=extended
 echo "[-] Installing node"
 nvm install 12
 
-echo "[-] Installing node stuffs"
+echo "[-] Installing node client for neovim"
 npm i -g neovim
+
+echo "[-] Installing trelew"
+npm i -g trelew
 
 echo "[-] Installing python3 extension for nvim"
 python3 -m pip install --user --upgrade pynvim
