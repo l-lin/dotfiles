bf7d0f1103c04402d4d2881a63e3687f517db766
Louis Lin <lin.louis@pm.me>
2022-10-21 18:43:41 +0200

    install: fix mvnd

--- installs/2-asdf.mvnd.sh
+++ installs/2-asdf.mvnd.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-mvnd_version="3.6.3"
+mvnd_version="0.8.2"
 
-if type mvn >/dev/null 2>&1; then
+if type mvnd >/dev/null 2>&1; then
   echo "[-] mvnd already installed => skipping"
 else
   echo "[-] installing mvnd ${mvnd_version}"
-  asdf plugin-add mvnd https://github.com/joschi/asdf-mvnd
+  asdf plugin-add mvnd https://github.com/joschi/asdf-mvnd | true
   asdf install mvnd "${mvnd_version}"
   asdf global mvnd "${mvnd_version}"
 fi
