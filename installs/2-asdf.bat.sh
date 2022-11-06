edffebcf1d675d3bdda903a7c1870608aaff3c8f
Louis Lin <lin.louis@pm.me>
2022-11-06 17:22:35 +0100

    install: upgrade bat

--- installs/2-asdf.bat.sh
+++ installs/2-asdf.bat.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-bat_version="0.17.1"
+bat_version="0.22.1"
 
 if type bat >/dev/null 2>&1; then
   echo "[-] bat already exists => skipping"
 else
   echo "[-] installing bat: https://github.com/sharkdp/bat"
-  asdf plugin add bat
+  asdf plugin add bat | true
   asdf install bat "${bat_version}"
   asdf global bat "${bat_version}"
 fi
