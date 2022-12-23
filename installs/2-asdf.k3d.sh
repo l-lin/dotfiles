009b8810082611e5fabfe8038349d0360a05fe7d
Louis Lin <lin.louis@pm.me>
2022-12-23 14:15:07 +0100

    install: bump k3d

--- installs/2-asdf.k3d.sh
+++ installs/2-asdf.k3d.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-k3d_version="5.4.1"
+k3d_version="5.4.6"
 
 if type k3d >/dev/null 2>&1; then
   echo "[-] k3d already installed => skipping"
 else
   echo "[-] installing k3d ${k3d_version}"
-  asdf plugin add k3d
+  asdf plugin add k3d | true
   asdf install k3d "${k3d_version}"
   asdf global k3d "${k3d_version}"
 fi
