3113b60acebbd282a2eef9df900466b2cd362f3d
Louis Lin <lin.louis@pm.me>
2023-02-06 17:08:15 +0100

    nvim: add keymap to commit

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,5 +1,6 @@
 local map = vim.api.nvim_set_keymap
 map('n', '<leader>gs', '<cmd>G<cr>', { noremap = true, desc = 'git status' })
+map('n', '<leader>gc', '<cmd>G commit<cr>', { noremap = true, desc = 'git commit' })
 map('n', '<leader>gp', '<cmd>G pull<cr>', { noremap = true, desc = 'git pull' })
 map('n', '<leader>gP', '<cmd>G push<cr>', { noremap = true, desc = 'git push' })
 map('n', '<leader>gf', '<cmd>G push --force-with-lease<cr>', { noremap = true, desc = 'git push --force-with-lease' })
