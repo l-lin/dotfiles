818564bfa6e53e09448c3762ca9427358d54a14d
Louis Lin <lin.louis@pm.me>
2023-02-09 23:50:06 +0100

    nvim: complete cmp config

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -9,10 +9,7 @@ cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
             require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
         end,
     },
     window = {
@@ -20,8 +17,8 @@ cmp.setup({
         -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        --['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        --['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
@@ -41,7 +38,7 @@ cmp.setup({
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
+              luasnip.jump(-1)
             else
                 fallback()
             end
@@ -62,9 +59,6 @@ cmp.setup({
 -- cmp configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
     sources = cmp.config.sources(
-        {
-            { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-        },
         {
             { name = 'buffer' },
         }
