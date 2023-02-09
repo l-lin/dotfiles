0ce0b096990933cb96e1e08c3e035e582ca6e2a2
Louis Lin <lin.louis@pm.me>
2023-02-09 10:38:23 +0100

    openbox: add TODO

--- openbox-themes/.config/openbox-themes/themes/gruvbox/apply.sh
+++ openbox-themes/.config/openbox-themes/themes/gruvbox/apply.sh
@@ -229,6 +229,8 @@ apply_obconfig () {
 	xmlstarlet ed -L -N a="$namespace" -u '/a:openbox_config/a:margins/a:left' -v ${ob_margin_l} "$config"
 	xmlstarlet ed -L -N a="$namespace" -u '/a:openbox_config/a:margins/a:right' -v ${ob_margin_r} "$config"
 
+  # TODO: remove shortcuts to C-A-m and C-S-R
+
 	# Reload Openbox Config
 	openbox --reconfigure
 }
