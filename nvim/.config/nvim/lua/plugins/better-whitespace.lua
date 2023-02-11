3570b04be1024a28ad685f2b17e1891c11a707ef
Louis Lin <lin.louis@pm.me>
2023-02-11 18:32:35 +0100

    nvim: config better-whitespace plugin

--- nvim/.config/nvim/lua/plugins/better-whitespace.lua
+++ nvim/.config/nvim/lua/plugins/better-whitespace.lua
@@ -1,6 +1,7 @@
 vim.g.better_whitespace_filetypes_blacklist = "['dashboard']"
 vim.cmd [[ highlight ExtraWhitespace ctermbg=78 ]]
 vim.api.nvim_set_keymap('n', '<leader>cw', ':StripWhitespace<CR>', { noremap = true, desc = 'Strip whitespace' })
-vim.api.nvim_set_keymap('n', '<leader>pw', ':ToggleWhitespace<CR>', { noremap = true, desc = 'Toggle whitespace' })
+vim.api.nvim_set_keymap('n', '<leader>cW', ':ToggleWhitespace<CR>', { noremap = true, desc = 'Toggle whitespace' })
+vim.g.better_whitespace_guicolor = '#cc241d'
 vim.g.better_whitespace_enabled = 0
-vim.g.strip_whitespace_on_save = 0
+vim.g.strip_whitespace_on_save = 1
