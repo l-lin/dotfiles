bfee34c7de119c13a65f145e9fb012ec27c3cff8
Louis Lin <lin.louis@pm.me>
2023-03-05 10:50:37 +0100

    nvim: add config to treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,19 @@
-require 'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup({
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = {
+    "c",
+    "help",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "query",
+    "vim",
+  },
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = false,
   highlight = {
     enable = true,
   },
@@ -7,4 +22,4 @@ require 'nvim-treesitter.configs'.setup {
     extended_mode = true,
     max_file_lines = nil
   }
-}
+})
