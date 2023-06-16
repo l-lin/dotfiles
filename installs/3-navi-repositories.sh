32046f5087c65ff37ee52aa28409d1da6088c3ae
Louis Lin <lin.louis@pm.me>
2023-06-16 10:14:27 +0200

    navi: use another repository for cheatsheets

---
+++ installs/3-navi-repositories.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu
+
+install_navi_repository() {
+  local vcs_source=$1
+  local user=$2
+  local repo=$3
+
+  local target_folder
+  target_folder="$(navi info cheats-path)/${user}__${repo}"
+
+  if [ -d "${target_folder}" ]; then
+    echo "[-] ${target_folder} already exists"
+  else
+    git clone "${vcs_source}:${user}/${repo}" "${target_folder}"
+  fi
+}
+
+install_navi_repository "git@github.com" l-lin cheats
+
