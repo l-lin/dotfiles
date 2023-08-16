09d637f53ba27668516b10cf365d30f5aa4fd311
Louis Lin <lin.louis@pm.me>
2023-08-16 22:06:25 +0200

    nvim(go): refactor config

--- nvim/.config/nvim/ftplugin/go.lua
+++ nvim/.config/nvim/ftplugin/go.lua
@@ -1,9 +1,8 @@
--- -------------------------------
--- KEYMAPS
--- -------------------------------
-local map = vim.keymap.set
-map("n", "<F46>", "<Cmd>lua require('neotest').run.run()<CR>",
-  { noremap = true, desc = "Execute test (Shift+F10)" })
-map("n", "<F45>", "<Cmd>lua require('neotest').run.run({strategy = 'dap'})<CR>",
-  { noremap = true, desc = "Debug test (Shift+F9)" })
+local function attach_keymaps()
+  local map = require("mapper").map
+  local bufopts = { noremap = true }
+  map("n", "<F46>", "<cmd>lua require('neotest').run.run()<cr>", bufopts, "Execute test (Shift+F10)")
+  map("n", "<F45>", "<Cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", bufopts, "Debug test (Shift+F9)")
+end
 
+attach_keymaps()
