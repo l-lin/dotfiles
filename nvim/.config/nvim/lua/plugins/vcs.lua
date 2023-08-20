f0cb86e8a123cbdd05d4ce1a75c9c49ff6ba9c95
Louis Lin <lin.louis@pm.me>
2023-08-20 23:11:13 +0200

    nvim(vcs): close git status with q

--- nvim/.config/nvim/lua/plugins/vcs.lua
+++ nvim/.config/nvim/lua/plugins/vcs.lua
@@ -1,12 +1,24 @@
 return {
   "tpope/vim-fugitive",
   keys = {
-    { "<leader>gs", "<cmd>G<cr>", desc = "git status" },
-    { "<leader>gc", "<cmd>G commit<cr>", desc = "git commit" },
-    { "<leader>gp", "<cmd>G pull<cr>", desc = "git pull" },
-    { "<leader>gP", "<cmd>G push<cr>", desc = "git push" },
+    { "<leader>gs", "<cmd>G<cr>",                         desc = "git status" },
+    { "<leader>gc", "<cmd>G commit<cr>",                  desc = "git commit" },
+    { "<leader>gp", "<cmd>G pull<cr>",                    desc = "git pull" },
+    { "<leader>gP", "<cmd>G push<cr>",                    desc = "git push" },
     { "<leader>gF", "<cmd>G push --force-with-lease<cr>", desc = "git push --force-with-lease" },
-    { "<leader>gb", "<cmd>G blame<cr>", desc = "git blame" },
-    { "<leader>gl", "<cmd>0GcLog<cr>", desc = "git log" },
+    { "<leader>gb", "<cmd>G blame<cr>",                   desc = "git blame" },
+    { "<leader>gl", "<cmd>0GcLog<cr>",                    desc = "git log" },
   },
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("fugitive_close_with_q", { clear = true }),
+      pattern = {
+        "fugitive",
+      },
+      callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+      end,
+    })
+  end,
 }
