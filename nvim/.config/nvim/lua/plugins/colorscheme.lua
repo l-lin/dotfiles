cec501cbe0f680b15b03e9b2a7c8fa1bec67bb01
Louis Lin <lin.louis@pm.me>
2023-08-20 10:29:52 +0200

    nvim(colorscheme): add gruvbox

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -14,10 +14,23 @@ return {
     },
   },
   {
+    "sainnhe/gruvbox-material",
+    enabled = false,
+    config = function()
+      vim.g.gruvbox_material_background = "medium" -- hard, soft, medium
+      vim.g.gruvbox_material_foreground = "material" -- original, mix, material
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_sign_column_background = "none"
+
+      vim.cmd([[ colorscheme gruvbox-material ]])
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "kanagawa",
       defaults = {
+        -- disable default keymaps as the window navigation overrides Navigator plugin's one
         keymaps = false,
       },
     },
