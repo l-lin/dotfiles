d6d6ab39e29a3b09dbf997295767f1919e88c36e
Louis Lin <lin.louis@pm.me>
2023-08-20 14:12:57 +0200

    nvim(dap): add telescope-dap

---
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -0,0 +1,3 @@
+-- Java convention is 4 spaces
+vim.o.tabstop = 4
+vim.o.shiftwidth = vim.o.tabstop
--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -116,5 +116,11 @@ return {
         desc = "Telescope DAP configuration",
       },
     },
+    dependencies = {
+      "nvim-telescope/telescope-dap.nvim",
+      config = function()
+        require("telescope").load_extension("dap")
+      end,
+    },
   },
 }
