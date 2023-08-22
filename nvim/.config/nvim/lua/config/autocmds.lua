17d658072d0b7a468fa680deda46e8786925814d
Louis Lin <lin.louis@pm.me>
2023-08-22 15:42:37 +0200

    nvim(vcs): add fugitiveblame to close_with_q autocmd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -5,6 +5,7 @@ vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
   pattern = {
     "fugitive",
+    "fugitiveblame",
     "git",
     "DiffviewFiles",
     "DiffviewFileHistory",
