e7a406f722e148ad0247ada0ad64429eeb954ec1
Louis Lin <lin.louis@pm.me>
2023-08-25 23:54:38 +0200

    nvim(dap): restructure ui

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -64,22 +64,20 @@ return {
       layouts = {
         {
           elements = {
-            {
-              id = "scopes",
-              size = 0.25,
-            },
-            { id = "repl",    size = 0.25 },
-            { id = "stacks",  size = 0.25 },
-            { id = "watches", size = 0.25 },
+            { id = "scopes",      size = 0.25 },
+            { id = "breakpoints", size = 0.25 },
+            { id = "stacks",      size = 0.25 },
+            { id = "watches",     size = 0.25 },
           },
           size = 40,
           position = "left",
         },
         {
           elements = {
-            "console",
+            { id = "repl",    size = 0.25 },
+            { id = "console", size = 0.75 },
           },
-          size = 10,
+          size = 15,
           position = "bottom",
         },
       },
