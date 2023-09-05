32723864411f41d01fec8c1737650a1bea5bfa3a
Louis Lin <lin.louis@pm.me>
2023-09-05 10:06:42 +0200

    nvim: no trailing space in visual mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -185,3 +185,6 @@ map("n", "<leader>vl", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
 -- NOTE: goto definition put here because for some reason, it's remapped to native <C-b>...
 map("n", "<C-b>", "<cmd>Telescope lsp_definitions<cr>", { desc = "Go to definition (Ctrl+b)" })
+
+-- do not include white space characters when using $ in visual mode, see https://vi.stackexchange.com/q/12607/15292
+map("x", "$", "g_")
