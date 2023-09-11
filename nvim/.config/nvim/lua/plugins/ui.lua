05aa1b2f90a362ff6274ba5691c7bbf1be5746ab
Louis Lin <lin.louis@pm.me>
2023-09-11 10:07:34 +0200

    nvim(ui): fix keymaps

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -12,6 +12,12 @@ return {
       --   },
       -- },
     },
+    keys = {
+      { "<c-f>", false },
+      { "<c-b>", false },
+      { "<c-d>", function() if not require("noice.lsp").scroll(4) then return "<c-d>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
+      { "<c-u>", function() if not require("noice.lsp").scroll(-4) then return "<c-u>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
+    }
   },
 
   -- navigate between neovim and multiplexers
