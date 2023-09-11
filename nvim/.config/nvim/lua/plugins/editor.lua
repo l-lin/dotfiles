6e593f3801f677982e4648df97c694bf9bf7f02d
Louis Lin <lin.louis@pm.me>
2023-09-11 10:07:22 +0200

    nvim(editor): better spectre opts

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -170,10 +170,14 @@ return {
     keys = {
       {
         "<M-3>",
-        function()
-          require("spectre").open()
-        end,
-        desc = "Replace in files (Spectre) (Alt+3)",
+        "<cmd>lua require('spectre').open_file_search()<cr>",
+        desc = "Replace in file (Spectre) (Alt+3)",
+      },
+      {
+        "<M-3>",
+        "<cmd>lua require('spectre').open_file_search({select_word = true})<cr>",
+        mode = "v",
+        desc = "Replace in file (Spectre) (Alt+3)",
       },
     },
     opts = {
