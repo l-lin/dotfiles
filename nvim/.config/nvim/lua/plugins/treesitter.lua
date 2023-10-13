83e58d23a118c299cc24ff87d5524466517691ca
Louis Lin <lin.louis@pm.me>
2023-10-13 15:51:52 +0200

    nvim(treesitter): context too big for java projects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,5 +2,13 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     cmd = { "TSInstallSync" },
-  }
+  },
+  -- Show context of the current function
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "LazyFile",
+    opts = {
+      multiline_threshold = 5,
+    },
+  },
 }
