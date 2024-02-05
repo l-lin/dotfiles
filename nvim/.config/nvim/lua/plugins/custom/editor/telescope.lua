9fa96285f1a3bf424a62bf748ac4ee9e60c1838e
Louis Lin <lin.louis@pm.me>
2024-02-05 13:24:00 +0100

    nvim(editor): revert telescope config
    Not working as I expected... Too much conflict with code actions + lsp
    references...

--- nvim/.config/nvim/lua/plugins/custom/editor/telescope.lua
+++ nvim/.config/nvim/lua/plugins/custom/editor/telescope.lua
@@ -11,23 +11,8 @@ local function get_selected_text()
   return text
 end
 
-local _last_picker = nil
----Resume Telescope search if picker is the same.
--- src: https://github.com/nvim-telescope/telescope.nvim/issues/1701#issuecomment-1817992007
----@param func function: the Telescope picker
----@param opts table: options to pass to the picker
-local function resume_search_if_same_picker(func, opts)
-  if func == _last_picker then
-    require("telescope.builtin").resume()
-  else
-    _last_picker = func
-    func(opts or {})
-  end
-end
-
 local M = {}
 
 M.get_selected_text = get_selected_text
-M.resume_search_if_same_picker = resume_search_if_same_picker
 
 return M
