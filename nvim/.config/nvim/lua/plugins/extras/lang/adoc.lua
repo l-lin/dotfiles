6c9ec98aad5f461bfce6894d32bd41ace83a0e33
Louis Lin <lin.louis@pm.me>
2024-03-22 11:48:04 +0100

    nvim(adoc): fix adoc ft

--- nvim/.config/nvim/lua/plugins/extras/lang/adoc.lua
+++ nvim/.config/nvim/lua/plugins/extras/lang/adoc.lua
@@ -4,7 +4,7 @@ return {
     cmd = {
       "AsciiDocPreview"
     },
-    ft = { "adoc" },
+    ft = { "asciidoc" },
     build = "cd server && npm install",
     keys = {
       { "<leader>ca", "<cmd>AsciiDocPreview<cr>", noremap = true, desc = "Start Asciidoc preview and open in web browser" },
