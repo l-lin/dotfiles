623a2f725b1647038c1a8c3cfe93324ae27f7f67
Louis Lin <lin.louis@pm.me>
2024-06-05 17:44:03 +0200

    nix: add hyprland

--- home-manager/modules/gui/wayland/hyprland.nix
+++ home-manager/modules/gui/wayland/hyprland.nix
@@ -1,4 +1,16 @@
 # Not working for me... :(
-{ ... }: {
-
+#
+# See:
+# - https://nixos.wiki/wiki/Hyprland
+# - https://wiki.hyprland.org/Nix/Hyprland-on-NixOS/
+{ pkgs, ... }: {
+  wayland.windowManager.hyprland = {
+    # Whether to enable Hyprland wayland compositor
+    enable = true;
+    # The hyprland package to use
+    package = pkgs.hyprland;
+    extraConfig = ''
+      ${builtins.readFile ./hyprland.conf}
+    '';
+  };
 }
--- nixos/modules/gui/wayland/hyprland.nix
+++ nixos/modules/gui/wayland/hyprland.nix
@@ -15,8 +15,6 @@
   };
 
   environment.sessionVariables = {
-    # If your cursor becomes invisible
-    WLR_NO_HARDWARE_CURSORS = "1";
     # Hint electron apps to use wayland
     NIXOS_OZONE_WL = "1";
   };
