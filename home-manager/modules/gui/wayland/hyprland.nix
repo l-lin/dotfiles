6847c3009ceec1bce250cecb3fe7973e154a3919
Louis Lin <lin.louis@pm.me>
2024-06-06 10:40:46 +0200

    hyprland: comment xdg-portal

--- home-manager/modules/gui/wayland/hyprland.nix
+++ home-manager/modules/gui/wayland/hyprland.nix
@@ -19,13 +19,13 @@
   ];
 
   # Enable screen sharing
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-    ];
-  };
+  #xdg.portal = {
+  #  enable = true;
+  #  wlr.enable = true;
+  #  extraPortals = [
+  #    pkgs.xdg-desktop-portal-gtk
+  #  ];
+  #};
 
   #environment.sessionVariables = {
   #  # Hint electron apps to use wayland
