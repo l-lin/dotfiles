08a12c78de4d6c4cba07e1545c4657f8d5bef265
Louis Lin <lin.louis@pm.me>
2024-06-07 20:40:52 +0200

    hm: add swaylock settings

--- home-manager/modules/gui/wm/wayland/swaylock/default.nix
+++ home-manager/modules/gui/wm/wayland/swaylock/default.nix
@@ -7,5 +7,22 @@
   programs.swaylock = {
     enable = true;
     package = pkgs.swaylock-effects;
+    settings = {
+      clock = true;
+      color = "00000000";
+      font = "JetBrainsMono Nerd Font";
+      show-failed-attempts = true;
+      indicator = true;
+      indicator-radius = 220;
+      indicator-thickness = 25;
+      separator-color = "00000000";
+      line-uses-ring = false;
+      grace = 0;
+      grace-no-mouse = true;
+      grace-no-touch = true;
+      datestr = "%d.%m.%y";
+      fade-in = "0.1";
+      ignore-empty-password = true;
+    };
   };
 }
