1544772decc319171437cddc3987215d2c44a91c
Louis Lin <lin.louis@pm.me>
2024-06-07 16:59:28 +0200

    hm: migrate some CLI tools

--- home-manager/modules/tui/cli.nix
+++ home-manager/modules/tui/cli.nix
@@ -4,13 +4,33 @@
 
 { pkgs, ... }: {
   home.packages = with pkgs; [
+    # Monitor resources: https://github.com/aristocratos/btop
+    btop
+    # CLI client for cheat.sh: https://github.com/chubin/cheat.sh
+    cht-sh
+    # Syntax highlighting for diff: https://www.colordiff.org/
+    colordiff
+    # Monitor container resources: https://ctop.sh/
+    ctop
+    # Command line HTTP client: https://curl.se/
     curl
+    # User-friendly find: https://github.com/sharkdp/fd
     fd
+    # GNU Compiler Collection.
     gcc
-    lsd
+    # Add `make` command.
     gnumake
+    # CLI to create JSON objects: https://github.com/jpmens/jo
+    jo
+    # User-friendly command line HTTP client: https://httpie.io/
+    httpie
+    # Better ls command: https://github.com/lsd-rs/lsd
+    lsd
+    # Pretty ping: https://github.com/denilsonsa/prettyping
+    prettyping
+    # Fastest grep in the wild: https://github.com/BurntSushi/ripgrep
     ripgrep
-    stow
+    # Command line tool for retrieving files using HTTP, HTTPS and FTP.
     wget
   ];
 }
