771cd36bc8268b7be22dff85380bfc971d900a58
Louis Lin <lin.louis@pm.me>
2024-06-08 19:34:00 +0200

    hm: switch eww config

--- home-manager/modules/gui/eww/config/scripts/wifi.sh
+++ home-manager/modules/gui/eww/config/scripts/wifi.sh
@@ -1,12 +1,67 @@
-#!/bin/sh
+#!/usr/bin/env bash
+STATUS=$(nmcli | grep wlo1 | awk 'FNR == 1 {print $2}')
+SSID=$(iwgetid | awk -F '"' '{ print $2 }')
+STRENGTH=$(awk 'NR==3 {printf("%.0f",$3*10/7)}' /proc/net/wireless)
 
-if iwctl station wlan0 show | grep -q "connected"; then
-    icon=""
-    ssid=Amadeus
-    status="Connected to ${ssid}"
-else
-    icon="睊"
-    status="offline"
+toggle() {
+    if [[ $STATUS == "connected" ]]; then
+        nmcli radio wifi off
+        notify-send --icon=window-close --urgency=normal "Wi-Fi" "Wi-Fi has been turned off!"
+    else
+        nmcli radio wifi on
+        notify-send --icon=checkmark --urgency=normal "Wi-Fi" "Wi-Fi has been turned on, you are back online!"
+    fi
+}
+
+
+class() {
+    if [[ $STATUS == "connected" ]]; then
+        echo active
+    else
+        echo inactive
+    fi
+}
+
+ssid() {
+    if [[ $STATUS == "connected" ]]; then
+        echo $(iwgetid -r)
+
+    else
+        echo "n/a"
+    fi
+}
+color() {
+    if [[ $STATUS == "connected" ]]; then
+        echo "green"
+
+    else
+        echo "red"
+    fi
+}
+icon() {
+    if [[ $STATUS == "connected" ]]; then
+        echo "󰤨 "
+
+    else
+        echo "󰤮 "
+    fi
+}
+speed() {
+  s1=$(awk 'NR==3 {print $3}''' /proc/net/wireless)
+  s2=${s1::-1}
+  echo $s2
+}
+
+if [[ $1 == "--toggle" ]]; then
+    toggle
+elif [[ $1 == "--class" ]]; then
+    class
+elif [[ $1 == "--ssid" ]]; then
+   ssid 
+elif [[ $1 == "--color" ]]; then
+   color 
+elif [[ $1 == "--icon" ]]; then
+   icon
+elif [[ $1 == "--speed" ]]; then
+   speed
 fi
-
-echo "{\"icon\": \"${icon}\", \"status\": \"${status}\"}" 
--- home-manager/modules/gui/eww/default.nix
+++ home-manager/modules/gui/eww/default.nix
@@ -21,25 +21,25 @@ in {
   };
 
   # Symlink to ~/.config/eww/style/_colors.scss
-  #xdg.configFile."eww/style/_colors.scss".text = ''
-  #  $background : #${palette.base00};
-  #  $foreground : #${palette.base05};
-  #  $background-alt : #${palette.base00};
-  #  $background-light : #${palette.base04};
-  #  $foreground-alt : #${palette.base05};
-  #  $red : #${palette.base08};
-  #  $red-light : #${palette.base08};
-  #  $green : #${palette.base0B};
-  #  $green-light : #${palette.base0B};
-  #  $yellow : #${palette.base0A};
-  #  $yellow-light : #${palette.base0A};
-  #  $blue : #${palette.base0D};
-  #  $blue-light : #${palette.base0D};
-  #  $cyan : #${palette.base0C};
-  #  $cyan-light : #${palette.base0C};
-  #  $magenta : #${palette.base0E};
-  #  $magenta-light : #${palette.base0E};
-  #  $comment : #${palette.base04};
-  #  $accent : #${palette.base0D};
-  #'';
+  xdg.configFile."eww/style/_colors.scss".text = ''
+    $background : #${palette.base00};
+    $foreground : #${palette.base05};
+    $background-alt : #${palette.base00};
+    $background-light : #${palette.base04};
+    $foreground-alt : #${palette.base05};
+    $red : #${palette.base08};
+    $red-light : #${palette.base08};
+    $green : #${palette.base0B};
+    $green-light : #${palette.base0B};
+    $yellow : #${palette.base0A};
+    $yellow-light : #${palette.base0A};
+    $blue : #${palette.base0D};
+    $blue-light : #${palette.base0D};
+    $cyan : #${palette.base0C};
+    $cyan-light : #${palette.base0C};
+    $magenta : #${palette.base0E};
+    $magenta-light : #${palette.base0E};
+    $comment : #${palette.base04};
+    $accent : #${palette.base0D};
+  '';
 }
