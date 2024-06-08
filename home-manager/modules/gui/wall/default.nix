bed4e68a9e3d6ff131b56ad775db87590b5fc16d
Louis Lin <lin.louis@pm.me>
2024-06-08 11:20:51 +0200

    eww: fix image

--- home-manager/modules/gui/wall/default.nix
+++ home-manager/modules/gui/wall/default.nix
@@ -4,5 +4,6 @@
 
 { ... }: {
   home.file."Pictures/hi.png".source = ./hi.png;
+  home.file."Pictures/zelda_wind_walker.png".source = ./zelda_wind_walker.png;
   home.file."Pictures/nix.png".source = ./nix.png;
 }
