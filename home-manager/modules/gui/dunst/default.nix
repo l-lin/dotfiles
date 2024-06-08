19284df1ee8d32f3a31215a1790d3ee20b744b29
Louis Lin <lin.louis@pm.me>
2024-06-08 21:12:03 +0200

    hm: update dunst config

--- home-manager/modules/gui/dunst/default.nix
+++ home-manager/modules/gui/dunst/default.nix
@@ -9,79 +9,44 @@ let
 in {
   services.dunst = {
     enable = true;
+
+    # For settings: man dunst.5
+    # also available here: https://github.com/dunst-project/dunst/blob/master/docs/dunst.5.pod
     settings = {
       global = {
         follow = "mouse";
-        width = "(180,500)";
-        origin = "bottom-center";
-        alignment = "left";
-        vertical_alignment = "center";
-        ellipsize = "middle";
-        offset = "0x45";
-        padding = 15;
-        horizontal_padding = 15;
-        text_icon_padding = 15;
-        icon_position = "left";
-        min_icon_size = 48;
-        max_icon_size = 64;
-        highlight = "#${palette.base06}";
-        progress_bar = true;
-        progress_bar_height = 12;
-        progress_bar_frame_width = 1;
-        progress_bar_min_width = 150;
-        progress_bar_max_width = 300;
-        separator_height = 2;
-        frame_width = 2;
-        frame_color = "#${palette.base06}";
-        separator_color = "frame";
-        corner_radius = 3;
-        transparency = 0;
-        gap_size = 8;
-        line_height = 0;
-        notification_limit = 0;
-        idle_threshold = 120;
-        history_length = 20;
-        show_age_threshold = 60;
-        markup = "full";
-        font = "Product Sans 12";
-        format = "<b>%s</b>\\n%b";
-        word_wrap = "yes";
-        sort = "yes";
-        shrink = "no";
-        indicate_hidden = "yes";
-        sticky_history = "yes";
-        ignore_newline = "no";
-        show_indicators = "no";
-        stack_duplicates = true;
-        always_run_script = true;
-        hide_duplicate_count = false;
-        icon_theme = "reversal";
-        ignore_dbusclose = false;
-        force_xwayland = false;
-        force_xinerama = false;
-        mouse_left_click = "do_action";
-        mouse_middle_click = "close_all";
-        mouse_right_click = "close_current";
+        # Dynamic width of the notification window, from 0 to 300px.
+        width = "(0,300)";
+        # The maximum height of a single notification, excluding the frame.
+        height = 300;
+        # Respectively the horizontal and vertical offset in pixels from the corner
+        # of the screen specified by origin. A negative offset will lead to the notification being off screen.
+        # Margin of 14px from the right and 50 pixels from the top.
+        offset = "14x50";
+
+        # The distance in pixels from the content to the border of the window in the horizontal axis.
+        horizontal_padding = 10;
+
+        # Defines color of the frame around the notification window.
+        frame_color = "#${palette.base05}";
+        background = "#${palette.base00}";
+        foreground = "#${palette.base05}";
+
+        font = "JetBrainsMono Nerd Font 12";
+
+        corner_radius = 4;
+        timeout = 4;
       };
 
-      fullscreen_delay_everything = { fullscreen = "delay"; };
-      urgency_low = {
-        timeout = 4;
-        background = "#${palette.base00}";
-        foreground = "#${palette.base05}";
-        highlight = "#${palette.base06}";
-      };
-      urgency_normal = {
-        timeout = 4;
-        background = "#${palette.base00}";
-        foreground = "#${palette.base05}";
-        highlight = "#${palette.base06}";
-      };
+      ##################
+      # Specific rules #
+      ##################
+
+      urgency_low = {};
+      urgency_normal = {};
       urgency_critical = {
-        timeout = 8;
-        background = "#${palette.base08}";
-        foreground = "#${palette.base06}";
-        highlight = "#${palette.base0A}";
+        frame_color = "#${palette.base08}";
+        timeout = 0;
       };
     };
   };
