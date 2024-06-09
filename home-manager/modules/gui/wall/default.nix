1c5924c3398c35d810d77647908368c21bdc1ff4
Louis Lin <lin.louis@pm.me>
2024-06-09 11:54:13 +0200

    hm: add cat wall

--- home-manager/modules/gui/wall/default.nix
+++ home-manager/modules/gui/wall/default.nix
@@ -2,7 +2,8 @@
 # Wallpapers.
 #
 
-{ ... }: {
+{
+  home.file."Pictures/cat.jpg".source = ./cat.jpg;
   home.file."Pictures/hi.png".source = ./hi.png;
   home.file."Pictures/zelda_wind_walker.png".source = ./zelda_wind_walker.png;
   home.file."Pictures/nix.png".source = ./nix.png;
