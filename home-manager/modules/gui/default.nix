aac89aea8401b25a805f39e60b843a1e67735380
Louis Lin <lin.louis@pm.me>
2024-06-09 16:30:37 +0200

    hm: add gimp

--- home-manager/modules/gui/default.nix
+++ home-manager/modules/gui/default.nix
@@ -18,6 +18,8 @@
   home.packages = with pkgs; [
     # Comprehensive, professional-quality productivity suite, a variant of openoffice.org: https://libreoffice.org/
     libreoffice
+    # The GNU Image Manipulation Program: https://www.gimp.org/
+    gimp
     # A simple multi-page document viewer for the MATE desktop: https://mate-desktop.org/
     mate.atril
     # A powerful knowledge base that works on top of a local folder of plain text Markdown files: https://obsidian.md/
