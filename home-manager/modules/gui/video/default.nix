8125e422ea3518612d31edd14d0a97fbc464f69f
Louis Lin <lin.louis@pm.me>
2024-06-09 20:36:17 +0200

    hm: add mpv

--- home-manager/modules/gui/video/default.nix
+++ home-manager/modules/gui/video/default.nix
@@ -4,6 +4,8 @@
 
 { pkgs, ... }: {
   home.packages = with pkgs; [
+    # General-purpose media player, fork of MPlayer and mplayer2: https://mpv.io/
+    mpv
     # Cross-platform media player and streaming server: https://www.videolan.org/vlc/
     vlc
   ];
