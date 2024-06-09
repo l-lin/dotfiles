be672d3c6ad583b448b206504d9651597e837f03
Louis Lin <lin.louis@pm.me>
2024-06-09 10:06:08 +0200

    lf: add fzf_search and progress on paste

--- home-manager/modules/tui/lf.nix
+++ home-manager/modules/tui/lf.nix
@@ -13,9 +13,42 @@
 
     # options: https://github.com/gokcehan/lf/blob/master/doc.md
     commands = {
+      fzf_search = ''
+        ''${{
+            RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+            res="$(
+                FZF_DEFAULT_COMMAND="$RG_PREFIX \'\'" \
+                    fzf --bind "change:reload:$RG_PREFIX {q} || true" \
+                    --ansi --layout=reverse --header 'Search in files' \
+                    | cut -d':' -f1 | sed 's/\\/\\\\/g;s/"/\\"/g'
+            )"
+            [ -n "$res" ] && lf -remote "send $id select \"$res\""
+        }}
+      '';
+
       pager = ''
         ''${{
           bat --paging=always "$f"
+        }}
+      '';
+
+      paste = ''
+        &{{
+            set -- $(cat ~/.local/share/lf/files)
+            mode="$1"
+            shift
+            case "$mode" in
+                copy)
+                    rsync -av --ignore-existing --progress -- "$@" . |
+                    stdbuf -i0 -o0 -e0 tr '\r' '\n' |
+                    while IFS= read -r line; do
+                        lf -remote "send $id echo $line"
+                    done
+                    ;;
+                move) mv -n -- "$@" .;;
+            esac
+            rm ~/.local/share/lf/files
+            lf -remote "send clear"
         }}
       '';
     };
@@ -23,6 +56,7 @@
     keybindings = {
       a = "push %mkdir<space>";
       d = "delete";
+      f = "fzf_search";
       gd = "cd ${config.xdg.userDirs.download}";
       o = "open";
       O = "pager";
