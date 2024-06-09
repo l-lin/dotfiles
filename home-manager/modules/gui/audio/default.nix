b98de52ab727b8f06404ff375c487951ca6edaf7
Louis Lin <lin.louis@pm.me>
2024-06-09 14:36:39 +0200

    hm: replace to pulsemixer

--- home-manager/modules/gui/audio/default.nix
+++ home-manager/modules/gui/audio/default.nix
@@ -2,9 +2,14 @@
 # Audio stuff.
 #
 
-{
+{ pkgs, ... }: {
   imports = [
     ./ncmpcpp.nix
-    ./pulseaudio.nix
+  ];
+
+  home.packages = with pkgs; [
+    # Cli and curses mixer for pulseaudio.
+    # src: https://github.com/GeorgeFilipkin/pulsemixer
+    pulsemixer
   ];
 }
--- home-manager/modules/gui/audio/pulseaudio.nix
+++
@@ -1,8 +0,0 @@
-#
-# Adjust the audio volume of the PulseAudio sound system
-# src: https://gitlab.xfce.org/panel-plugins/xfce4-pulseaudio-plugin
-#
-
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ xfce.xfce4-pulseaudio-plugin ];
-}
