0866f3939614898727e98bc00c7d9fdef1b42caa
Louis Lin <lin.louis@pm.me>
2024-06-11 15:21:48 +0200

    hm: add pulseaudio

--- home-manager/modules/tui/audio/default.nix
+++ home-manager/modules/tui/audio/default.nix
@@ -11,6 +11,8 @@
   home.packages = with pkgs; [
     # A minimalist command line interface to MPD: https://www.musicpd.org/clients/mpc/
     mpc-cli
+    # PulseAudio is a sound server for POSIX and Win32 systems: http://www.pulseaudio.org/
+    pulseaudio
     # CLI and curses mixer for pulseaudio.
     # src: https://github.com/GeorgeFilipkin/pulsemixer
     pulsemixer
