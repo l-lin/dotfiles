6c06b6c4219f0813e3050a2604db93de1b346ce7
Louis Lin <lin.louis@pm.me>
2024-06-15 10:34:52 +0200

    yazi: override config file to set EDITOR
    For some reason, the floating windows are always using nano...

--- home-manager/modules/file-manager/yazi/default.nix
+++ home-manager/modules/file-manager/yazi/default.nix
@@ -3,9 +3,12 @@
 # src: https://yazi-rs.github.io/
 #
 
-{
+{ userSettings, ...}: {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
   };
+
+  # symlink to ~/.config/yazi/yazi.toml
+  xdg.configFile."yazi/yazi.toml".source = ./config/yazi.toml;
 }
