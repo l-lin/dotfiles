15dc03b8f4031c9299f99aa35169b2cc87d8952e
Louis Lin <lin.louis@pm.me>
2024-06-15 21:06:59 +0200

    tmux: configure color scheme from home-manager

--- home-manager/modules/shell/multiplexer/tmux/default.nix
+++ home-manager/modules/shell/multiplexer/tmux/default.nix
@@ -4,13 +4,29 @@
 # src: https://github.com/tmux/tmux/wiki
 #
 
-{ pkgs, ...}: {
+{ config, pkgs, userSettings, ...}: {
   programs.tmux = {
     enable = true;
     # Cannot set the shell directly in the tmux.conf file as the binary is not in /bin folder.
     shell = "${pkgs.zsh}/bin/zsh";
     extraConfig = ''
-      ${builtins.readFile ./config/.tmux.conf}
+      ${builtins.readFile ./config/tmux.conf}
     '';
   };
+
+  # Symlinks to ~/.config/tmux/
+  xdg.configFile."tmux/plugins.conf".source = ./config/plugins.conf;
+  xdg.configFile."tmux/colorscheme.conf".text = ''
+# Colorscheme (must be set before other plugins)
+set -g @plugin 'l-lin/tmux-colorscheme'
+set -g @tmux-colorscheme '${userSettings.theme}'
+set -g @tmux-colorscheme-show-pomodoro true
+set -g @tmux-colorscheme-show-upload-speed true
+set -g @tmux-colorscheme-show-download-speed true
+set -g @tmux-colorscheme-show-prefix-highlight true
+set -g @tmux-colorscheme-show-battery true
+set -g @tmux-colorscheme-show-cpu true
+set -g @tmux-colorscheme-show-cpu-temp true
+set -g @tmux-colorscheme-show-ram true
+  '';
 }
