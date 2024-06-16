6713ba055320593fd4046e8dde42b0d25c0f6c72
Louis Lin <lin.louis@pm.me>
2024-06-16 22:14:55 +0200

    nvim: add nix lang support

--- home-manager/modules/editor/nvim/config/lua/config/lazy.lua
+++ home-manager/modules/editor/nvim/config/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     --{ import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.nix" },
     --{ import = "lazyvim.plugins.extras.lang.rust" },
     --{ import = "lazyvim.plugins.extras.lang.terraform" },
     --{ import = "lazyvim.plugins.extras.lang.typescript" },
