49eb0a0b746e41f7473b0f2f7206434c52501e9d
Louis Lin <lin.louis@pm.me>
2024-06-29 11:16:09 +0200

    lazygit: set delta polarity using theme
    Delta `--detect-dark-light` does not seem to work seamlessly... Had to
    force the polarity from the theme config.

--- home-manager/modules/vcs/lazygit/default.nix
+++ home-manager/modules/vcs/lazygit/default.nix
@@ -3,9 +3,27 @@
 # src: https://github.com/jesseduffield/lazygit
 #
 
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   home.packages = with pkgs; [ lazygit ];
 
   # Symlink to ~/.config/lazygit/config.yml
-  xdg.configFile."lazygit/config.yml".source = ./config/config.yml;
+  xdg.configFile."lazygit/config.yml".text = ''
+# https://github.com/jesseduffield/lazygit/blob/master/docs/Config.md
+gui:
+  border: "rounded"
+  showBottomLine: false
+  showRandomTip: false
+  nerdFontsVersion: "3"
+git:
+  mainBranches: [master, main, develop]
+  paging:
+    colorArg: always
+    pager: delta --${config.theme.polarity} --paging=never
+keybinding:
+  universal:
+    prevPage: "<c-u>"
+    nextPage: "<c-d>"
+    gotoTop: "g"
+    gotoBottom: "G"
+  '';
 }
