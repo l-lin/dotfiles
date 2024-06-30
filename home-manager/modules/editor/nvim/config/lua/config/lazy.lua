97c7b2a2f8bc824faa163bcda851343bdbe05d79
Louis Lin <lin.louis@pm.me>
2024-06-30 12:41:52 +0200

    nvim: add git support

--- home-manager/modules/editor/nvim/config/lua/config/lazy.lua
+++ home-manager/modules/editor/nvim/config/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.nix" },
