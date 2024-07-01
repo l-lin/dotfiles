079417322a1fb63b0760aafa99d46a4d819274db
Louis Lin <lin.louis@pm.me>
2024-07-01 18:05:42 +0200

    zsh: remove asdf from path

--- home-manager/modules/shell/zsh/config/conf.d/asdf.zsh
+++ home-manager/modules/shell/zsh/config/conf.d/asdf.zsh
@@ -4,5 +4,6 @@
 #
 
 # add asdf bin / shims to $PATH
-path=(${ASDF_DIR}/{bin,shims}(N) $path)
+# NOTE: disable it because I'm using Nix, i.e. no need for asdf
+#path=(${ASDF_DIR}/{bin,shims}(N) $path)
 
