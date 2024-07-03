ef2a0dd43b4a5c608a2b7716f9497d420ec2dc7c
Louis Lin <lin.louis@pm.me>
2024-07-03 14:26:03 +0200

    security: add encryption tool age

--- home-manager/modules/security/default.nix
+++ home-manager/modules/security/default.nix
@@ -6,6 +6,8 @@
   imports = fileExplorer.allSubdirs ./.;
 
   home.packages = with pkgs; [
+    # Modern encryption tool with small explicit keys: https://age-encryption.org/
+    age
     # Simple and flexible tool for managing secrets: https://github.com/getsops/sops
     sops
   ];
