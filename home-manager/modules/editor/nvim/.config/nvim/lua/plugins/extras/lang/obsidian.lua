0721128d54e30223fc7568048a76825ffed39f8c
Louis Lin <lin.louis@pm.me>
2024-07-04 09:51:10 +0200

    nvim: add obsidian.nvim

---
+++ home-manager/modules/editor/nvim/.config/nvim/lua/plugins/extras/lang/obsidian.lua
@@ -0,0 +1,56 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    "BufReadPre " .. vim.fn.expand "~" .. "/perso/notes/**.md",
+    "BufNewFile " .. vim.fn.expand "~" .. "/perso/notes/**.md",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "perso",
+        path = "~/perso/notes",
+        overrides = {
+          notes_subdir = "6-triage",
+        }
+      },
+    },
+
+    daily_notes = {
+      folder = "5-rituals/daily/2024",
+      date_format = "%Y-%m-%d",
+      default_tags = { "journal/daily" },
+      template = "0-meta/templates/template-daily.md",
+    },
+
+    completion = {
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+
+    -- Where to put new notes. Valid options are
+    --  * "current_dir" - put new notes in same directory as the current buffer.
+    --  * "notes_subdir" - put new notes in the default notes subdirectory.
+    new_notes_location = "notes_subdir",
+
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = "telescope.nvim",
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+    },
+  },
+}
