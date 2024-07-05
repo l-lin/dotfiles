23140b2be5294e027def3cdae4b55fe53014fca2
Louis Lin <lin.louis@pm.me>
2024-07-05 09:13:35 +0200

    cloud: add terraform

--- home-manager/modules/cloud/default.nix
+++ home-manager/modules/cloud/default.nix
@@ -2,6 +2,11 @@
 # Cloud related stuff.
 #
 
-{ fileExplorer, ... }: {
+{ fileExplorer, pkgs, ... }: {
   imports = fileExplorer.allSubdirs ./.;
+
+  home.packages = with pkgs; [
+    # Tool for building, changing, and versioning infrastructure: https://www.terraform.io/
+    terraform
+  ];
 }
