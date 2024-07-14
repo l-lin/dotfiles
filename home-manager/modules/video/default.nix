c92edc19c362ac9a68bc1695849153b4454fb096
Louis Lin <lin.louis@pm.me>
2024-07-14 12:11:58 +0200

    video: add wf-recorder

--- home-manager/modules/video/default.nix
+++ home-manager/modules/video/default.nix
@@ -10,5 +10,7 @@
     mpv
     # Cross-platform media player and streaming server: https://www.videolan.org/vlc/
     vlc
+    # Utility program for screen recording of wlroots-based compositors: https://github.com/ammen99/wf-recorder
+    wf-recorder
   ];
 }
