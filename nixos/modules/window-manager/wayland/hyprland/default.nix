e21bc5d9a7df5791f8db9f8c4a336f551f7035ed
Louis Lin <lin.louis@pm.me>
2024-07-22 09:10:49 +0200

    nix(wayland): add xdg-desktop-portal-hyprland

--- nixos/modules/window-manager/wayland/hyprland/default.nix
+++ nixos/modules/window-manager/wayland/hyprland/default.nix
@@ -9,7 +9,8 @@
 { pkgs, ... }: {
   programs.hyprland.enable = true;
 
-  # Enable XDG desktop integration.
+  # Enable XDG Desktop Portal (XDP) integration.
+  # An XDP is a program that lets other applications communicate swiftly with the compositor through D-Bus.
   # Used for:
   # - screen sharing
   # - communication with GTK-based application (e.g. set properties like "prefer-dark")
@@ -19,6 +20,14 @@
 
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
+      # For Hyprland, you’d usually use xdg-desktop-portal-wlr.
+      # Unfortunately, due to various reasons the -wlr portal is inferior to the KDE or Gnome ones.
+      # In order to bridge the gap, Hyprland has its own fork of xdg-desktop-portal-wlr that has more features,
+      # called xdg-desktop-portal-hyprland.
+      # You don’t need xdg-desktop-portal-hyprland. Hyprland will work with xdg-desktop-portal-wlr, but
+      # xdg-desktop-portal-hyprland has more features, like e.g. window sharing.
+      # src: https://wiki.hyprland.org/hyprland-wiki/pages/Useful-Utilities/Hyprland-desktop-portal/
+      xdg-desktop-portal-hyprland
     ];
   };
 
