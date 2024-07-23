9f28920845ba0ecbc165df494217263088a3ec9c
Louis Lin <lin.louis@pm.me>
2024-07-23 10:38:26 +0200

    temporal: add temporal-cli with its zsh completions

---
+++ home-manager/modules/misc/temporal/default.nix
@@ -0,0 +1,11 @@
+#
+# Command-line interface for running Temporal Server and interacting with Workflows,
+# Activities, Namespaces, and other parts of Temporal.
+# src: https://docs.temporal.io/cli
+#
+
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ temporal-cli ];
+
+  xdg.configFile."zsh/completions/_temporal".source = ./.config/zsh/completions/_temporal;
+}
