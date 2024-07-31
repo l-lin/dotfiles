6f313237140c3306530c649bddf8aaeb370a271a
Louis Lin <lin.louis@pm.me>
2024-07-31 10:17:27 +0200

    navi(tmux): use popup instead of split-window

--- home-manager/modules/misc/navi/default.nix
+++ home-manager/modules/misc/navi/default.nix
@@ -10,6 +10,8 @@
     NAVI_FZF_OVERRIDES_VAR = "--preview-window top:50%:wrap:border";
   };
 
-  # Symlink to ~/.config/navi/config.yaml
+  # Symlinks
   xdg.configFile."navi/config.yaml".source = ./.config/navi/config.yaml;
+  xdg.configFile."tmux/plugins/navi/navi.tmux".source = ./.config/tmux/plugins/navi/navi.tmux;
+  xdg.configFile."zsh/plugins/navi/navi.plugin.zsh".source = ./.config/zsh/plugins/navi/navi.plugin.zsh;
 }
