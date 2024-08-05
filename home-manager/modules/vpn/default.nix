944261ad48ae2700ce6e7821b0eb42a204ff397d
Louis Lin <lin.louis@pm.me>
2024-08-05 21:08:52 +0200

    vpn: disable openfortivpn

--- home-manager/modules/vpn/default.nix
+++ home-manager/modules/vpn/default.nix
@@ -3,10 +3,10 @@
 #
 
 { outputs, pkgs, systemSettings, ... }: {
-  home.packages = with pkgs; [
-    # Client for PPP+SSL VPN tunnel services: https://github.com/adrienverge/openfortivpn
-    openfortivpn
-
-    outputs.packages.${systemSettings.system}.openfortivpn-webview
-  ];
+  # home.packages = with pkgs; [
+  #   # Client for PPP+SSL VPN tunnel services: https://github.com/adrienverge/openfortivpn
+  #   openfortivpn
+  #
+  #   outputs.packages.${systemSettings.system}.openfortivpn-webview
+  # ];
 }
