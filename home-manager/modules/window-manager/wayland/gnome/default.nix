5d0826e0d6e6337c8a6716c8d3e0a3f94d488297
Louis Lin <lin.louis@pm.me>
2024-08-06 15:29:47 +0200

    gnome: update keybindings

--- home-manager/modules/window-manager/wayland/gnome/default.nix
+++ home-manager/modules/window-manager/wayland/gnome/default.nix
@@ -15,10 +15,81 @@
     # gnomeExtensions.just-perfection
     # # Keyboard-driven layer for GNOME Shell: https://github.com/pop-os/shell
     # gnomeExtensions.pop-shell
+
+    # Copy/paste utilities: https://github.com/bugaevc/wl-clipboard
+    wl-clipboard
   ];
   dconf = {
     enable = true;
     settings = {
+      # org.gnome.mutter -------------------------------------------
+
+      "org/gnome/mutter" = {
+        # Disable Super to trigger window overview
+        overlay-key = "";
+      };
+
+      # org.gnome.desktop -------------------------------------------
+
+      "org/gnome/desktop/wm/keybindings" = {
+        close = ["<Super>q"];
+        toggle-maximized = ["<Super>f"];
+
+        minimize = []; # default was Super-h => used by tile-left-half-ignore-ta.
+
+        # Move window to monitors.
+        move-to-monitor-left = ["<Super><Shift>h"];
+        move-to-monitor-right = ["<Super><Shift>l"];
+
+        switch-input-source = ["XF86Keyboard"]; # disable Super-space => used by toggle-application-view.
+        switch-input-source-backward = ["<Shift>XF86Keyboard"];
+      };
+      "org/gnome/desktop/interface" = {
+        clock-show-weekday = true;
+        show-battery-percentage = true;
+      };
+      "org/gnome/desktop/wm/preferences" = {
+        # Buttons top right to minimize, maximize or close the window.
+        button-layout = ":minimize,maximize,close";
+
+        # Only visually alert, no need to have sound polution.
+        audible-bell = false;
+        visual-bell = true;
+        visual-bell-type = "frame-flash";
+
+        # Focus on window under mouse cursor.
+        auto-raise-delay = 0;
+        auto-raise = true;
+        focus-mode = "mouse";
+      };
+
+      # org.gnome.settings-daemon -------------------------------------------
+
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        screenreader = []; # default was <Alt><Super>s => used by screenshot-window.
+        screensaver = ["<Super>w"];
+
+        terminal = "disabled";
+
+        custom-keybindings = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        ];
+      };
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        name = "kitty";
+        # TODO: fix kitty installation from home-manager then replace with `kitty` instead.
+        command = "/home/${userSettings.username}/.local/kitty.app/bin/kitty -e tmux -2 -u";
+        binding = "<Super>t";
+      };
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+        name = "file-explorer";
+        command = "nautilus";
+        binding = "<Super>e";
+      };
+
+      # org.gnome.shell -------------------------------------------
+
       "org/gnome/shell" = {
         favorite-apps = ["obsidian.desktop" "kitty.desktop" "firefox.desktop"];
         welcome-dialog-last-shown-version = "";
@@ -44,48 +115,17 @@
       #   enable-intellihide = false;
       # };
       "org/gnome/shell/keybindings" = {
+        # Screenshot.
         show-screenshot-ui = ["Print" "<Super>s"];
         screenshot-window = ["<Alt>Print" "<Super><Shift>s"];
 
+        # Display all application view.
+        toggle-application-view = ["<Super>space"];
       };
       "org/gnome/shell/extensions/tiling-assistant" = {
+        # Move window to half of screen.
         tile-left-half-ignore-ta = ["<Super>h"];
         tile-right-half-ignore-ta = ["<Super>l"];
-      };
-
-      "org/gnome/desktop/interface" = {
-        clock-show-weekday = true;
-        show-battery-percentage = true;
-      };
-      "org/gnome/desktop/wm/preferences" = {
-        button-layout = ":minimize,maximize,close";
-      };
-      "org/gnome/desktop/wm/keybindings" = {
-        close = ["<Super>q"];
-        toggle-maximized = ["<Super>f"];
-        minimize = [];
-      };
-      "org/gnome/settings-daemon/plugins/media-keys" = {
-        screensaver = ["<Super>w"];
-
-        terminal = "disabled";
-
-        custom-keybindings = [
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
-        ];
-      };
-
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        name = "kitty";
-        # TODO: fix kitty installation from home-manager then replace with `kitty` instead.
-        command = "/home/${userSettings.username}/.local/kitty.app/bin/kitty -e tmux -2 -u";
-        binding = "<Super>t";
-      };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-        name = "file explorer";
-        command = "nautilus";
-        binding = "<Super>e";
       };
     };
   };
