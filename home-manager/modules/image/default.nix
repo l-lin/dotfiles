2ab24c990ee1238f769a08cb299e0bafc8389df4
Louis Lin <lin.louis@pm.me>
2024-08-07 16:49:40 +0200

    hyprland: move hyprpicker to hyprland

--- home-manager/modules/image/default.nix
+++ home-manager/modules/image/default.nix
@@ -6,14 +6,6 @@
   imports = fileExplorer.allSubdirs ./.;
 
   home.packages = with pkgs; [
-    # A wlroots-compatible Wayland color picker that does not suck.
-    #
-    # /!\ Need to set the cursor theme for hyprpicker to work.
-    # See https://github.com/hyprwm/hyprpicker/issues/51.
-    # Cursor set in `theme/default.nix`.
-    #
-    # src: https://github.com/hyprwm/hyprpicker
-    hyprpicker
     # The GNU Image Manipulation Program: https://www.gimp.org/
     gimp
     # A command line image viewer for tiling window managers: https://sr.ht/~exec64/imv/
