e0c448526ad4547475a295817807cebb66a64698
Louis Lin <lin.louis@pm.me>
2024-09-27 21:43:08 +0200

    nvim(ruby): add fuzzy_ruby_server as LSP server along with ruby-lsp
    I don't really understand why sometimes `ruby-lsp` does not work...
    At least, `fuzzy_ruby_server` always works, so... let's keep them both.

--- stow/nvim/.config/nvim/lua/plugins/extras/lang/ruby.lua
+++ stow/nvim/.config/nvim/lua/plugins/extras/lang/ruby.lua
@@ -1,4 +1,49 @@
 return {
+  -- Alternative LSP server to ruby-lsp for navigation (no auto-completion).
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "pheen/fuzzy_ruby_server" },
+    opts = {
+      servers = {
+        fuzzy_ls = {
+          init_options = {
+            allocationType = "tempdir",
+            indexGems = false,
+            reportDiagnostics = false,
+          },
+        },
+      },
+      setup = {
+        fuzzy_ls = function(_, opts)
+          local lspconfig = require("lspconfig")
+          local configs = require("lspconfig.configs")
+
+          if not configs.fuzzy_ls then
+            configs.fuzzy_ls = {
+              default_config = {
+                cmd = { vim.fn.expand("~/.local/share/nvim/lazy/fuzzy_ruby_server/bin/fuzzy_x86_64-unknown-linux-gnu") },
+                filetypes = { "ruby" },
+                root_dir = function(fname)
+                  return lspconfig.util.find_git_ancestor(fname)
+                end,
+                settings = {},
+                init_options = {
+                  -- possible values:
+                  -- ram: use RAM (can be very high on big project)
+                  -- tempdir: use mmap directory to store the indexes (e.g. /tmp/.tmpcCUkiK)
+                  allocationType = "ram",
+                  indexGems = true,
+                  reportDiagnostics = true,
+                },
+              },
+            }
+          end
+          lspconfig.fuzzy_ls.setup(opts)
+        end,
+      },
+    },
+  },
+
   -- Neotest adapter for Minitest
   {
     "nvim-neotest/neotest",
@@ -10,12 +55,15 @@ return {
       adapters = {
         ["neotest-minitest"] = {
           test_cmd = function()
-            return vim.iter({
-              "bundle",
-              "exec",
-              "rails",
-              "test"
-            }):flatten():totable()
+            return vim
+              .iter({
+                "bundle",
+                "exec",
+                "rails",
+                "test",
+              })
+              :flatten()
+              :totable()
           end,
         },
       },
