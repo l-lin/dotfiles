2e0c304ffce436c4fca51e0dcf86309ff4512c43
Louis Lin <lin.louis@pm.me>
2024-09-30 22:36:55 +0200

    nvim: replace J keymap to remove trailing whitespace

--- stow/nvim/.config/nvim/lua/config/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/keymaps.lua
@@ -10,10 +10,17 @@ map("n", "<leader>yf", "<cmd>let @+=expand('%')<CR>", { noremap = true, desc = "
 map("n", "<leader>yF", "<cmd>let @+=expand('%:p')<CR>", { noremap = true, desc = "Copy current buffer absolute path to clipboard" })
 map("n", "<leader>yn", "<cmd>let @+=expand('%:t')<CR>", { noremap = true, desc = "Copy current buffer file name to clipboard" })
 
+-- remove keymaps set globally by LazyVim
+-- use default H and L to navigate
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
+
 -- navigation
 -- Always put cursor at middle of screen.
 map("n", "<C-d>", "<C-d>zz", { noremap = true })
 map("n", "<C-u>", "<C-u>zz", { noremap = true })
+map("n", "<S-h>", "<S-h>zz", { noremap = true })
+map("n", "<S-l>", "<S-l>zz", { noremap = true })
 
 -- search
 -- Always put cursor at middle of screen.
@@ -49,7 +56,11 @@ map({ "n", "v" }, "<M-C-L>", function()
   require("lazyvim.util").format({ force = true })
 end, { desc = "Format" })
 
--- remove keymaps set globally by LazyVim
--- use default H and L to navigate
-vim.keymap.del("n", "<S-h>")
-vim.keymap.del("n", "<S-l>")
+-- Remove trailing whitespace when it's an open parenthesis
+map(
+  "n",
+  "J",
+  "<cmd>lua if string.sub(vim.api.nvim_get_current_line(), -1, -1) == '(' then vim.api.nvim_command('norm! Jx') else vim.api.nvim_command('norm! J') end<cr>",
+  { noremap = true, silent = true, desc = "Join line without whitespace if it's an open parenthesis" }
+)
+
