f347a79215179ca79c2797bd79316a0ffdda0010
Louis Lin <lin.louis@pm.me>
2024-10-06 10:47:30 +0200

    style: add zenwritten-light theme

---
+++ home-manager/modules/style/themes/zenwritten-light/default.nix
@@ -0,0 +1,50 @@
+#
+# 🪨 A collection of contrast-based Vim/Neovim colorschemes.
+# src: https://github.com/zenbones-theme/zenbones.nvim
+#
+
+{ pkgs, ... }:
+let
+  polarity = "light";
+in {
+
+  theme = {
+    inherit polarity;
+    nvimColorScheme = "zenwritten";
+    nvimColorSchemePluginLua = ''
+{
+  "zenbones-theme/zenbones.nvim",
+  dependencies = "rktjmp/lush.nvim",
+}
+    '';
+  };
+
+  stylix = {
+    inherit polarity;
+    # stylix needs a background image when gnome is enable.
+    image = ../../../image/wallpaper/pictures/sky.png;
+    cursor = {
+      # Material Based Cursor Theme: https://github.com/ful1e5/Bibata_Cursor
+      name = "Bibata-Modern-Classic";
+      size = 24;
+      package = pkgs.bibata-cursors;
+    };
+  };
+
+  gtk = {
+    iconTheme = {
+      # Pixel perfect icon theme for Linux: https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
+      name = "Papirus-Light";
+      package = pkgs.papirus-icon-theme;
+    };
+    theme = {
+      # Soothing pastel theme for GTK: https://github.com/catppuccin/gtk
+      name = "Catppuccin-Latte-Compact-Blue-Light";
+      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/themes/catppuccin-gtk/default.nix
+      package = pkgs.catppuccin-gtk.override {
+        size = "compact";
+        variant = "latte";
+      };
+    };
+  };
+}
