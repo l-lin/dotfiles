09e51eb94f2771676856fc2537c72ec121887768
Louis Lin <lin.louis@pm.me>
2024-10-08 21:31:28 +0200

    style: add grey light theme

---
+++ home-manager/modules/style/themes/grey/default.nix
@@ -0,0 +1,45 @@
+#
+# A light theme for NeoVim, with a light grey background 
+# src: https://github.com/yorickpeterse/nvim-grey
+#
+
+{ pkgs, ... }:
+let
+  polarity = "light";
+in {
+
+  theme = {
+    inherit polarity;
+    nvimColorScheme = "grey";
+    nvimColorSchemePluginLua = "{ 'yorickpeterse/nvim-grey' }";
+  };
+
+  stylix = {
+    inherit polarity;
+    # stylix needs a background image when gnome is enable.
+    image = ../../../image/wallpaper/pictures/sky.png;
+    cursor = {
+      # Material Based Cursor Theme: https://github.com/ful1e5/Bibata_Cursor
+      name = "Bibata-Modern-Classic";
+      size = 24;
+      package = pkgs.bibata-cursors;
+    };
+  };
+
+  gtk = {
+    iconTheme = {
+      # Pixel perfect icon theme for Linux: https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
+      name = "Papirus-Light";
+      package = pkgs.papirus-icon-theme;
+    };
+    theme = {
+      # Soothing pastel theme for GTK: https://github.com/catppuccin/gtk
+      name = "Catppuccin-Latte-Compact-Blue-Light";
+      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/themes/catppuccin-gtk/default.nix
+      package = pkgs.catppuccin-gtk.override {
+        size = "compact";
+        variant = "latte";
+      };
+    };
+  };
+}
