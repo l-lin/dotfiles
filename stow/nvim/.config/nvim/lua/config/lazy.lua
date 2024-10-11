52151f4218f55a78ca70fb60fde65b04b07864e9
Louis Lin <lin.louis@pm.me>
2024-10-11 22:04:56 +0200

    nvim: disable octo (not used)

--- stow/nvim/.config/nvim/lua/config/lazy.lua
+++ stow/nvim/.config/nvim/lua/config/lazy.lua
@@ -41,7 +41,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.util.dot" },
-    { import = "lazyvim.plugins.extras.util.octo" },
+    --{ import = "lazyvim.plugins.extras.util.octo" },
     { import = "lazyvim.plugins.extras.util.project" },
     -- import/override with your plugins
     { import = "plugins" },
