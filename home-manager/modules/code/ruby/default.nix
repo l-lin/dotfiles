994b29d5ec506e689f407ac5b78f759af3c645c1
Louis Lin <lin.louis@pm.me>
2024-11-01 23:24:41 +0100

    ruby: move env variable to its own nix file

---
+++ home-manager/modules/code/ruby/default.nix
@@ -0,0 +1,15 @@
+#
+# A dynamic, open source programming language with a focus on simplicity and
+# productivity. It has an elegant syntax that is natural to read and easy to
+# write.
+# src: https://www.ruby-lang.org/en/
+#
+
+{
+  home.sessionVariables = {
+    # auto-detect Gemfile in the current or parent directory, to avoid prefixing
+    # all ruby/gem commands with "bundle exec" src:
+    # https://ruby-doc.org/3.3.5/stdlibs/rubygems/Gem.html
+    RUBYGEMS_GEMDEPS = "-";
+  };
+}
