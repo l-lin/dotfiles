3f329542810137c4b50e5698b1e6846c7df3cd09
Louis Lin <lin.louis@pm.me>
2024-11-02 14:03:52 +0100

    zsh: remove obsolete files

--- home-manager/modules/shell/zsh/.config/zsh/conf.d/asdf.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-#
-# asdf support
-#
-
-# add asdf bin / shims to $PATH
-# NOTE: disable it because I'm using Nix, i.e. no need for asdf
-#path=(${ASDF_DIR}/{bin,shims}(N) $path)
-
--- home-manager/modules/shell/zsh/.config/zsh/conf.d/prompt.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-#
-# prompt
-#
-
-[[ ! -f ${ZDOTDIR}/.p10k.zsh ]] || source ${ZDOTDIR}/.p10k.zsh
-
