2e6a3addc92b36d939daad59df61b38c0a534e23
Louis Lin <lin.louis@pm.me>
2024-11-22 21:08:34 +0100

    nvim(ruby): open engine directory with mini.files

--- stow/nvim/.config/nvim/lua/plugins/extras/lang/ruby.lua
+++ stow/nvim/.config/nvim/lua/plugins/extras/lang/ruby.lua
@@ -1,3 +1,17 @@
+local function find_engines ()
+  local relative_filepath = vim.fn.expand("%:.")
+  local _, extension = relative_filepath:match("(.+)%.(.+)$")
+
+  if extension == "rb" then
+    local parts = vim.fn.split(relative_filepath, "/")
+    if #parts > 2 and parts[1] == "engines" then
+      return "engines/" .. parts[2]
+    end
+  end
+
+  return vim.api.nvim_buf_get_name(0)
+end
+
 return {
   -- Alternative LSP server to ruby-lsp for navigation (no auto-completion).
   {
@@ -44,7 +58,7 @@ return {
     },
   },
 
-  -- Neotest adapter for Minitest
+  -- Neotest adapter for Minitest.
   {
     "nvim-neotest/neotest",
     optional = true,
@@ -66,6 +80,22 @@ return {
               :totable()
           end,
         },
+      },
+    },
+  },
+
+  -- Navigate and manipulate file system.
+  {
+    "echasnovski/mini.files",
+    optional = true,
+    keys = {
+      {
+        "<leader>fh",
+        function()
+          require("mini.files").open(find_engines(), true)
+        end,
+        desc = "Open mini.files in current Ruby engine",
+        remap = true,
       },
     },
   },
