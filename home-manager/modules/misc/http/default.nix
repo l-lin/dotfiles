aeb86e27628117d13d687a56718f1f6b87f07f67
Louis Lin <lin.louis@pm.me>
2024-12-09 08:11:53 +0100

    http: add oha for http load testing

--- home-manager/modules/misc/http/default.nix
+++ home-manager/modules/misc/http/default.nix
@@ -10,6 +10,8 @@
     grpcurl
     # HTTP load generator, ApacheBench (ab) replacement: https://github.com/rakyll/hey
     hey
+    # HTTP load generator inspired by rakyll/hey with tui animation: https://github.com/hatoo/oha
+    oha
     # WebSockets client: https://github.com/vi/websocat
     websocat
     # CLI for retrieving files using HTTP, HTTPS and FTP.
