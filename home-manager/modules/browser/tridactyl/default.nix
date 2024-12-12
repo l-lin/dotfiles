ce6ee557b6e943acc60d88d4441c9ae71f6f4c6a
Louis Lin <lin.louis@pm.me>
2024-12-12 22:00:56 +0100

    tridactyl: unbind keymap

--- home-manager/modules/browser/tridactyl/default.nix
+++ home-manager/modules/browser/tridactyl/default.nix
@@ -13,6 +13,8 @@ let
   profileName = userSettings.username;
 in {
   # Symlink to ~/.config/tridactyl/tridactylrc
+  # Default keymaps: https://github.com/tridactyl/tridactyl/blob/8e4525a758dbf23c59af64f9ae3a5dacb633cb23/src/lib/config.ts#L132
+  # To find what keymap is bind to, press :bindshow <your_keymap>
   xdg.configFile."tridactyl/tridactylrc".text = ''
 " open nvim instead of default 'auto', which opens gvim (shortcut: Ctrl+i)
 set editorcmd ${userSettings.term} -e nvim %f '+normal!%lGzv%c|'
@@ -27,6 +29,10 @@ bind K tabprev
 bind J tabnext
 bind H back
 bind L forward
+
+" UNBINDS
+" With zen browser, this keymap is used to open web panel, and I don't often pin tabs.
+unbind <A-p>
 
 set profiledir ${config.home.homeDirectory}/${vendorPath}/${profileName}
   '';
