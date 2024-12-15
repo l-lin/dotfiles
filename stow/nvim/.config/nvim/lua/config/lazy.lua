21a5a3766eef3a196d02248dcde90949457e99e3
Louis Lin <lin.louis@pm.me>
2024-12-15 15:35:47 +0100

    nvim(coding): remove luasnip + convert snippets to VSCode-like

--- stow/nvim/.config/nvim/lua/config/lazy.lua
+++ stow/nvim/.config/nvim/lua/config/lazy.lua
@@ -13,7 +13,6 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     --{ import = "lazyvim.plugins.extras.coding.codeium" },
-    { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
