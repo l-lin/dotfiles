ff65f3f8aaf34b895aa2d6a0c1465561770dc344
Louis Lin <lin.louis@pm.me>
2024-12-29 13:56:23 +0100

    nvim(editor): remove cmp integration in emoji

--- stow/nvim/.config/nvim/lua/plugins/extras/editor/emoji.lua
+++ stow/nvim/.config/nvim/lua/plugins/extras/editor/emoji.lua
@@ -3,7 +3,6 @@ return {
     "allaman/emoji.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     cmd = { "Emoji" },
-    opts = { enable_cmp_integration = true },
     keys = {
       { "<leader>se", "<cmd>Emoji<cr>", noremap = true, silent = true, desc = "Emoji" },
       { "<M-;>", mode = "i", "<cmd>Emoji<cr>", desc = "Emoji" },
