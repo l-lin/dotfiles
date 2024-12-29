e630473bba1d505cc5db27dbb67b284bbbca7d2b
Louis Lin <lin.louis@pm.me>
2024-12-29 13:54:19 +0100

    nvim(editor): add global note

--- stow/nvim/.config/nvim/lua/plugins/extras/editor/global-note.lua
+++ stow/nvim/.config/nvim/lua/plugins/extras/editor/global-note.lua
@@ -43,12 +43,29 @@ local open_file_under_cursor = function(window_id)
   end
 end
 
+local post_open = function(buffer_id, window_id)
+  -- Add keymap on this buffer to press q to close the note.
+  vim.keymap.set("n", "q", function()
+    vim.api.nvim_win_close(window_id, true)
+  end, { buffer = buffer_id })
+
+  -- Remap gf to open the file under cursor in the current buffer, not
+  -- in the floating window.
+  vim.keymap.set("n", "gf", function()
+    open_file_under_cursor(window_id)
+  end, { buffer = buffer_id })
+end
+
 return {
   --  Opens global note in a float window
   {
     "backdround/global-note.nvim",
-    cmd = { "ProjectNote" },
+    cmd = { "GlobalNote", "ProjectNote" },
     opts = {
+      filename = "global.md",
+      directory = get_target_directory(),
+      command_name = "GlobalNote",
+      post_open = post_open,
       additional_presets = {
         project_local = {
           command_name = "ProjectNote",
@@ -57,23 +74,13 @@ return {
           filename = function()
             return get_project_name() .. ".md"
           end,
-          post_open = function(buffer_id, window_id)
-            -- Add keymap on this buffer to press q to close the note.
-            vim.keymap.set("n", "q", function()
-              vim.api.nvim_win_close(window_id, true)
-            end, { buffer = buffer_id })
-
-            -- Remap gf to open the file under cursor in the current buffer, not
-            -- in the floating window.
-            vim.keymap.set("n", "gf", function()
-              open_file_under_cursor(window_id)
-            end, { buffer = buffer_id })
-          end,
+          post_open = post_open,
         },
       },
     },
     keys = {
       { "<leader>cn", "<cmd>ProjectNote<cr>", noremap = true, silent = true, desc = "Open project notes" },
+      { "<leader>cN", "<cmd>GlobalNote<cr>", noremap = true, silent = true, desc = "Open global notes" },
     },
   },
 }
