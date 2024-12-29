dfb33d09c8ef8c6ac699a8f18679793d1763dae1
Louis Lin <lin.louis@pm.me>
2024-12-29 11:49:55 +0100

    nvim(lang): add maven

--- stow/nvim/.config/nvim/lua/config/lazy.lua
+++ stow/nvim/.config/nvim/lua/config/lazy.lua
@@ -63,6 +63,7 @@ require("lazy").setup({
     { import = "plugins.extras.lang.java" },
     { import = "plugins.extras.lang.just" },
     { import = "plugins.extras.lang.markdown" },
+    { import = "plugins.extras.lang.maven" },
     { import = "plugins.extras.lang.ruby" },
     { import = "plugins.extras.lang.typescript" },
     { import = "plugins.extras.lang.xml" },
---
+++ stow/nvim/.config/nvim/lua/plugins/extras/lang/maven.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "oclay1st/maven.nvim",
+    cmd = { "Maven", "MavenInit", "MavenExec" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {},
+  },
+}
