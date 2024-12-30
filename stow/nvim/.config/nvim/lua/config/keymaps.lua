fb679285b57d50be87ceab09006835466c931496
Louis Lin <lin.louis@pm.me>
2024-12-30 10:27:32 +0100

    nvim(keymaps): center cursor to middle of screen when navigating with count

--- stow/nvim/.config/nvim/lua/config/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event.
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua.
 -- Add any additional keymaps here.
+
+---Put the cursor in the middle of the screen, only when navigating with count.
+---No need to move the whole screen when moving line by line.
+---@param key string either 'j' or 'k'
+local function cursor_to_middle_of_screen(key)
+  if vim.v.count > 1 then
+    vim.api.nvim_command("norm! " .. vim.v.count .. key .. "zz")
+  else
+    vim.api.nvim_command("norm! " .. key)
+  end
+end
+
+---Remove the trailing whitespace when joining line if the last character is a
+---open parenthesis.
+local function remove_trailing_whitespace()
+  local last_char = string.sub(vim.api.nvim_get_current_line(), -1, -1)
+  if last_char == "(" then
+    vim.api.nvim_command("norm! Jx")
+  else
+    vim.api.nvim_command("norm! J")
+  end
+end
+
 local map = vim.keymap.set
 
 -- buffer
@@ -25,9 +48,9 @@ map("n", "<S-h>", "<S-h>zz", { noremap = true })
 map("n", "<S-l>", "<S-l>zz", { noremap = true })
 map("n", "{", "{zz", { noremap = true })
 map("n", "}", "}zz", { noremap = true })
-
+map("n", "j", function() cursor_to_middle_of_screen("j") end, { noremap = true })
+map("n", "k", function() cursor_to_middle_of_screen("k") end, { noremap = true })
 -- search
--- Always put cursor at middle of screen.
 map("n", "n", "nzzzv", { noremap = true })
 map("n", "N", "Nzzzv", { noremap = true })
 
@@ -35,7 +58,7 @@ map("n", "N", "Nzzzv", { noremap = true })
 map("n", "]<tab>", "<cmd>tabnext<cr>", { noremap = true, desc = "Next Tab" })
 map("n", "[<tab>", "<cmd>tabprevious<cr>", { noremap = true, desc = "Previous Tab" })
 
--- editing
+-- editing (same behavior as Intellij)
 map("n", "<C-y>", "dd", { noremap = true, desc = "Delete line" })
 
 -- use different buffer for delete and paste
@@ -49,8 +72,7 @@ vim.api.nvim_create_user_command('CheatSheet', function()
   vim.cmd('split ' .. vim.fn.expand(vim.fn.stdpath("config") .. '/doc/cheat_sheet.txt'))
 end, {})
 
--- Diagnostics
--- Same behavior as IntelliJ.
+-- diagnostics (same behavior as IntelliJ)
 map("n", "<F25>", vim.diagnostic.open_float, { desc = "Line Diagnostics (Ctrl+F1)" })
 map("n", "<F2>", vim.diagnostic.goto_next, { desc = "Next diagnostic (F2)" })
 
@@ -58,15 +80,8 @@ map("n", "<F2>", vim.diagnostic.goto_next, { desc = "Next diagnostic (F2)" })
 map("x", "$", "g_")
 
 -- format
-map({ "n", "v" }, "<M-C-L>", function()
-  require("lazyvim.util").format({ force = true })
-end, { desc = "Format" })
+map({ "n", "v" }, "<M-C-L>", function() require("lazyvim.util").format({ force = true }) end, { desc = "Format" })
 
--- Remove trailing whitespace when it's an open parenthesis
-map(
-  "n",
-  "J",
-  "<cmd>lua if string.sub(vim.api.nvim_get_current_line(), -1, -1) == '(' then vim.api.nvim_command('norm! Jx') else vim.api.nvim_command('norm! J') end<cr>",
-  { noremap = true, silent = true, desc = "Join line without whitespace if it's an open parenthesis" }
-)
+-- remove trailing whitespace when it's an open parenthesis
+map("n", "J", remove_trailing_whitespace, { noremap = true, silent = true, desc = "Join line without whitespace if it's an open parenthesis" })
 
