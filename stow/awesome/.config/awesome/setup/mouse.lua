2af770ce2a67d70c66df769ee9a756ff7037b56f
Louis Lin <lin.louis@pm.me>
2025-02-21 09:08:38 +0100

    awesome: remove mouse keybindings

--- stow/awesome/.config/awesome/setup/mouse.lua
+++ stow/awesome/.config/awesome/setup/mouse.lua
@@ -1,16 +1,4 @@
-local client, root = client, root
-
-local awful = require("awful")
-local gears = require("gears")
-
-root.buttons(gears.table.join(
-  -- awful.button({}, 3, function()
-  --   mymainmenu:toggle()
-  -- end),
-  awful.button({}, 4, awful.tag.viewnext),
-  awful.button({}, 5, awful.tag.viewprev)
-))
--- }}}
+local client = client, root
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
