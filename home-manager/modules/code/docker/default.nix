669e4d0118407aa3a4fc8b2d5240a4a95b565a34
Louis Lin <lin.louis@pm.me>
2025-03-08 16:28:31 +0100

    docker: add dive

--- home-manager/modules/code/docker/default.nix
+++ home-manager/modules/code/docker/default.nix
@@ -5,6 +5,8 @@
 
 { pkgs, ... }: {
   home.packages = with pkgs; [
+    # Tool for exploring each layer in a docker image: https://github.com/wagoodman/dive
+    dive
     # Docker CLI plugin to define and run multi-container applications with Docker: https://docs.docker.com/compose/
     docker-compose
     # A simple terminal UI for both docker and docker-compose: https://github.com/jesseduffield/lazydocker
