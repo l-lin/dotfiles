40f117bb20fa98e4a02490fc66639c7c4eb86b1d
Louis Lin <lin.louis@pm.me>
2025-03-09 10:35:02 +0100

    java: fix font rendering on Java desktop apps

--- home-manager/modules/code/java/default.nix
+++ home-manager/modules/code/java/default.nix
@@ -5,14 +5,22 @@
 
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    jdk21
     # Build automation tool (used primarily for Java projects): https://maven.apache.org/
     maven
   ];
 
+  programs.java = with pkgs; {
+    # Ensure JAVA_HOME is set.
+    enable = true;
+    package = jdk21;
+  };
+
   home.sessionVariables = {
     MAVEN_OPTS = "-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=WARN -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss";
     JDTLS_JVM_ARGS = "-javaagent:$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar";
+    # Better font rendering on Java desktop apps.
+    # src: https://wiki.nixos.org/wiki/Java
+    _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
   };
 
   # Symlink ~/.local/share/eclipse/java-code-style.xml
