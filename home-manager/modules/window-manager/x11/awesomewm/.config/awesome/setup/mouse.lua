ee13183ab719be2b7a541016407a817db575b338
Louis Lin <lin.louis@pm.me>
2025-03-16 19:01:55 +0100

    awesome: re-enable following mouse

--- home-manager/modules/window-manager/x11/awesomewm/.config/awesome/setup/mouse.lua
+++ home-manager/modules/window-manager/x11/awesomewm/.config/awesome/setup/mouse.lua
@@ -1,6 +1,6 @@
--- local client = client
---
--- -- Enable sloppy focus, so that focus follows mouse.
--- client.connect_signal("mouse::enter", function(c)
---   c:emit_signal("request::activate", "mouse_enter", { raise = false })
--- end)
+local client = client
+
+-- Enable sloppy focus, so that focus follows mouse.
+client.connect_signal("mouse::enter", function(c)
+  c:emit_signal("request::activate", "mouse_enter", { raise = false })
+end)
