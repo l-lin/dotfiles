7d04c6c865647dfa27d9e7b23fd583f9ea46de1f
Louis Lin <lin.louis@pm.me>
2025-03-20 14:05:49 +0100

    ai: configure ollama to use acceleration

--- home-manager/modules/ai/default.nix
+++ home-manager/modules/ai/default.nix
@@ -3,7 +3,10 @@
 #
 
 { pkgs, ... }: {
-  services.ollama.enable = true;
+  services.ollama = {
+    acceleration = "rocm";
+    enable = true;
+  };
   home.packages = with pkgs; [
     #  Go manage your Ollama models: https://github.com/sammcj/gollama
     gollama
