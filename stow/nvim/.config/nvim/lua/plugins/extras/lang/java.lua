22896b97ad6a6aef5faf815db728e6a99227e03d
Louis Lin <lin.louis@pm.me>
2025-03-21 21:26:09 +0100

    java: fix comment

--- stow/nvim/.config/nvim/lua/plugins/extras/lang/java.lua
+++ stow/nvim/.config/nvim/lua/plugins/extras/lang/java.lua
@@ -2,8 +2,8 @@ local java_configurer = require("plugins.custom.lang.java")
 -- Fetch Java related libraries from another registry (main mason registry does not have the java-test, lombok and spring-boot-tools).
 -- To install a specific version of a package installed by Mason:
 -- 1. edit .local/share/nvim/mason/registries/github/mason-org/mason-registry/registry.json
--- 3. uninstall the LSP/DAP/Whatever from Mason UI
--- 2. search for your LSP/DAP/Whatever and put your version
+-- 2. uninstall the LSP/DAP/Whatever from Mason UI
+-- 3. search for your LSP/DAP/Whatever and put your version
 -- 4. re-open Neovim
 -- src: https://github.com/rochakgupta/dotfiles/blob/e68d52d5b33b9d93e13ca8db8c63c04745cdd995/.config/nvim/lua/rochakgupta/plugins/nvim-lspconfig/servers.lua#L5-L20
 local mason_registries = { "github:nvim-java/mason-registry", "github:mason-org/mason-registry" }
