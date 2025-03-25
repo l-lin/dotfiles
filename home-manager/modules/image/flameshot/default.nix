120d77f9a4adf67ff84e443c7d6374459b301313
Louis Lin <lin.louis@pm.me>
2025-03-25 21:00:37 +0100

    flameshot: remove tray icon

--- home-manager/modules/image/flameshot/default.nix
+++ home-manager/modules/image/flameshot/default.nix
@@ -9,10 +9,30 @@ let
 in {
   home.packages = with pkgs; [ flameshot ];
 
+  # Example: https://github.com/flameshot-org/flameshot/blob/master/flameshot.example.ini
   xdg.configFile."flameshot/flameshot.ini".text = with palette; ''
 [General]
+;; Main UI color
+;; Color is any valid hex code or W3C color name
 uiColor=${base0D-hex}
+
+;; Last used color
+;; Color is any valid hex code or W3C color name
 drawColor=${base05-hex}
+
+;; Image Save Path
 savePath=${config.xdg.userDirs.pictures}/Screenshots
+
+;; Show the help screen on startup (bool)
+showHelp=false
+
+;; Show desktop notifications (bool)
+showDesktopNotification=false
+
+;; Whether the tray icon is disabled (bool)
+disabledTrayIcon=true
+
+;; Automatically close daemon when it's not needed (not available on Windows)
+autoCloseIdleDaemon=true
 '';
 }
