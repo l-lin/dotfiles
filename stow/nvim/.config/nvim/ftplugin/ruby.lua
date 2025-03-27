22561fd91fb0372d1df196d19602a21b6fec6009
Louis Lin <lin.louis@pm.me>
2025-03-27 20:42:49 +0100

    nvim(ruby): keymap to execute all tests

--- stow/nvim/.config/nvim/ftplugin/ruby.lua
+++ stow/nvim/.config/nvim/ftplugin/ruby.lua
@@ -22,7 +22,19 @@ vim.keymap.set(
       include_line_number = false,
     })
   end,
-  { buffer = true, desc = "Run all test" }
+  { buffer = true, desc = "Run all test (Alt+Shift+F9)" }
+)
+vim.keymap.set(
+  "n",
+  "<M-S-F9>",
+  function()
+    ruby.execute_file({
+      cmd = "rails t",
+      use_interactive_shell = true,
+      include_line_number = false,
+    })
+  end,
+  { buffer = true, desc = "Run all test (Alt+Shift+F9)" }
 )
 vim.keymap.set(
   "n",
