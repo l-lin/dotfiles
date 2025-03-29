3a30ee9ebee16051c9fd71dcc17ada4a36502741
Louis Lin <lin.louis@pm.me>
2025-03-29 23:02:29 +0100

    ai: add api keys

--- home-manager/modules/ai/default.nix
+++ home-manager/modules/ai/default.nix
@@ -4,4 +4,7 @@
 
 { fileExplorer, ... }: {
   imports = fileExplorer.allSubdirs ./.;
+
+  # Symlink ~/.config/zsh/functions/aider-convention-scraper.
+  xdg.configFile."zsh/zprofile.d/.zprofile.ai".source = ./.config/zsh/zprofile.d/.zprofile.ai;
 }
