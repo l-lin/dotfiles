1a04f2b51f97e1a2042f92d75f8e564f00097409
Louis Lin <lin.louis@pm.me>
2025-04-09 16:18:56 +0200

    nvim: re-enable Ctrl+F4

--- stow/nvim/.config/nvim/lua/config/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/keymaps.lua
@@ -28,8 +28,7 @@ local map = vim.keymap.set
 
 -- buffer
 -- Same behavior as browsers (muscle memory).
--- No Ctrl+F4 in MacOS...
---map("n", "<F28>", "<cmd>bd<CR>", { noremap = true, silent = true, desc = "Close current buffer (Ctrl+F4)" })
+map("n", "<F28>", "<cmd>bd<CR>", { noremap = true, silent = true, desc = "Close current buffer (Ctrl+F4)" })
 
 -- yank file path / name
 map("n", "<leader>yf", "<cmd>let @+=expand('%:.')<CR>", { noremap = true, desc = "Copy current buffer relative path to clipboard" })
