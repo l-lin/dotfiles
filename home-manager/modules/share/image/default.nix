4ad35d8dc13d9d37d4514f7cd4a1a5a8f1bed366
Louis Lin <lin.louis@pm.me>
2025-04-12 11:53:28 +0200

    image: install gimp on macOS

--- home-manager/modules/share/image/default.nix
+++ home-manager/modules/share/image/default.nix
@@ -2,15 +2,8 @@
 # Image related tools.
 #
 
-{ fileExplorer, pkgs, ... }: {
+{ fileExplorer, ... }: {
   imports = fileExplorer.allSubdirs ./.;
-
-  home.packages = with pkgs; [
-    # The GNU Image Manipulation Program: https://www.gimp.org/
-    gimp
-    # A command-line application to view images from the terminal written in Rust: https://github.com/atanunq/viu
-    viu
-  ];
 
   # ~/.face is used by waybar to set the user icon.
   home.file.".face".source = ./pictures/hi.png;
