27d2e0e1ebcbb62cf5a426a3e280e27f2b647855
Louis Lin <lin.louis@pm.me>
2025-04-29 22:19:48 +0200

    nix: disable direnv

--- home-manager/modules/share/code/direnv/default.nix
+++ home-manager/modules/share/code/direnv/default.nix
@@ -3,27 +3,31 @@
 # src: https://github.com/nix-community/nix-direnv
 #
 
-{ pkgs, ... }: {
-  programs.direnv = {
-    enable = true;
-    # Whether to enable a faster, persistent implementation of use_nix and use_flake, to replace the built-in one.
-    nix-direnv.enable = true;
-    # Whether to enable silent mode, that is, disabling direnv logging.
-    silent = true;
-  };
+# { pkgs, ... }: {
+#   programs.direnv = {
+#     enable = true;
+#     # Whether to enable a faster, persistent implementation of use_nix and use_flake, to replace the built-in one.
+#     nix-direnv.enable = true;
+#     # Whether to enable silent mode, that is, disabling direnv logging.
+#     silent = true;
+#   };
+#
+#   home.packages = with pkgs; [
+#     (writeShellScriptBin "nixify" ''
+#       ${builtins.readFile ./scripts/nixify.sh}
+#     '')
+#     (writeShellScriptBin "flakify" ''
+#       ${builtins.readFile ./scripts/flakify.sh}
+#     '')
+#   ];
+#
+#   # Symlink ~/.config/zsh/plugins/direnv
+#   xdg.configFile."zsh/plugins/direnv" = {
+#     source = ./.config/zsh/plugins/direnv;
+#     recursive = true;
+#   };
+# }
 
-  home.packages = with pkgs; [
-    (writeShellScriptBin "nixify" ''
-      ${builtins.readFile ./scripts/nixify.sh}
-    '')
-    (writeShellScriptBin "flakify" ''
-      ${builtins.readFile ./scripts/flakify.sh}
-    '')
-  ];
-
-  # Symlink ~/.config/zsh/plugins/direnv
-  xdg.configFile."zsh/plugins/direnv" = {
-    source = ./.config/zsh/plugins/direnv;
-    recursive = true;
-  };
-}
+# Using mise instead, which is has a better DX.
+# direnv needs to add the flake.nix to be in git stage area...
+{}
