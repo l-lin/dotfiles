80fc95ab714bcfecf90cd00f381340d3e5db8c1c
Louis Lin <lin.louis@pm.me>
2025-05-15 11:45:39 +0200

    code: add mermaid-cli

---
+++ home-manager/modules/share/code/mermaid/default.nix
@@ -0,0 +1,8 @@
+#
+# Generation of diagrams from text in a similar manner as markdown
+# src: https://github.com/mermaid-js/mermaid-cli
+#
+
+{
+  xdg.configFile."zsh/functions/mermaidify".source = ./.config/zsh/functions/mermaidify;
+}
