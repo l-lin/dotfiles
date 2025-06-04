07a4bcba23d471e54a3f199c0211cdb412a89f77
Louis Lin <lin.louis@pm.me>
2025-06-04 16:55:32 +0200

    editor: enable idea-community

--- home-manager/modules/share/editor/jetbrains/default.nix
+++ home-manager/modules/share/editor/jetbrains/default.nix
@@ -3,9 +3,11 @@
 # src: https://www.jetbrains.com/
 #
 
-{
-  # HACK: DISABLED because RubyMine does not seem to find rbenv...
-  # home.packages = with pkgs; [ jetbrains-toolbox ];
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    # Free Java, Kotlin, Groovy and Scala IDE from jetbrains (built from source): https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/applications/editors/jetbrains/default.nix#L53
+    jetbrains.idea-community
+  ];
 
   # Symlink to ~/.ideavimrc
   home.file.".ideavimrc".source = ./.ideavimrc;
