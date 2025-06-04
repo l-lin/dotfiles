0f3210ca676248676b81fcf358075799cd5006da
Louis Lin <lin.louis@pm.me>
2025-06-04 14:32:00 +0200

    java: disable flag _JAVA_OPTIONS as it's making nvim-lint break on kotlin

--- home-manager/modules/share/code/java/default.nix
+++ home-manager/modules/share/code/java/default.nix
@@ -14,12 +14,12 @@
   home.sessionVariables = {
     MAVEN_OPTS = "-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=WARN -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss";
     JDTLS_JVM_ARGS = "-javaagent:$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar";
-    # Better font rendering on Java desktop apps.
+    # Better font rendering on Java desktop apps with the flag `-Dawt.useSystemAAFontSettings=lcd`.
     # src: https://wiki.nixos.org/wiki/Java
     #
-    # Make macOS M4 works on Java 21.
+    # Make macOS M4 works on Java 21 with the flag `-XX:UseSVE=0`, but it's not really working...
     # src: https://github.com/corretto/corretto-21/issues/85#issuecomment-2537411974
-    _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd -XX:UseSVE=0";
+    #_JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd -XX:UseSVE=0";
   };
 
   # Symlink ~/.local/share/eclipse/java-code-style.xml
