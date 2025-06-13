d9cdb6ffcf3fe8b252e77a986d3b02dc4ba2c81b
Louis Lin <lin.louis@pm.me>
2025-06-13 14:50:17 +0200

    nvim(octo): change keymaps

--- stow/nvim/.config/nvim/lua/plugins/extras/vcs/octo.lua
+++ stow/nvim/.config/nvim/lua/plugins/extras/vcs/octo.lua
@@ -6,13 +6,23 @@ return {
   --  Edit and review GitHub issues and pull requests from the comfort of your favorite editor
   {
     "pwntester/octo.nvim",
+    dependencies = {
+      {
+        "folke/which-key.nvim",
+        opts = {
+          spec = {
+            { "<leader>go", group = "octo" },
+          },
+        },
+      },
+    },
     keys = {
       { "<leader>gp", false },
       { "<leader>gP", false },
       { "<leader>gr", false },
       { "<leader>gS", false },
 
-      { "<leader>gl", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
+      { "<leader>gop", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
       { "<leader>vR", "<cmd>Octo thread resolve<CR>", desc = "Resolve thread (Octo)", ft = "octo" },
     },
   },
