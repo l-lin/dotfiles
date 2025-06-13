c034af2338f2570bc518057dcee169518fe51474
Louis Lin <lin.louis@pm.me>
2025-06-13 22:51:34 +0200

    nvim(gitcommit): add codeblock keymap

---
+++ stow/nvim/.config/nvim/lua/plugins/custom/lang/markdown.lua
@@ -0,0 +1,17 @@
+local function add_codeblock_keymap()
+  vim.keymap.set("i", "<A-c>", function()
+    local row = unpack(vim.api.nvim_win_get_cursor(0))
+    local lines = {
+      "```",
+      "",
+      "```",
+    }
+    vim.api.nvim_buf_set_lines(0, row, row, false, lines)
+    vim.api.nvim_win_set_cursor(0, { row + 2, 0 })
+    vim.cmd("startinsert!")
+  end, { buffer = true, desc = "Add codeblock" })
+end
+
+local M = {}
+M.add_codeblock_keymap = add_codeblock_keymap
+return M
