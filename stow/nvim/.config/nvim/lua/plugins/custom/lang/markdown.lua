472b0f1e11ccb7241bb4d62d2bbf9c35260a5e19
Louis Lin <lin.louis@pm.me>
2025-06-28 16:20:51 +0200

    nvim(editor): fix codebock keymap

--- stow/nvim/.config/nvim/lua/plugins/custom/lang/markdown.lua
+++ stow/nvim/.config/nvim/lua/plugins/custom/lang/markdown.lua
@@ -3,11 +3,10 @@ local function add_codeblock_keymap()
     local row = unpack(vim.api.nvim_win_get_cursor(0))
     local lines = {
       "```",
-      "",
       "```",
     }
     vim.api.nvim_buf_set_lines(0, row, row, false, lines)
-    vim.api.nvim_win_set_cursor(0, { row + 2, 0 })
+    vim.api.nvim_win_set_cursor(0, { row + 1, 3 })
     vim.cmd("startinsert!")
   end, { buffer = true, desc = "Add codeblock" })
 end
