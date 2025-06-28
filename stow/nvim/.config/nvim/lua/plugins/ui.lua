f29f9511f93682ccdd388a9ab95ad05b40f57213
Louis Lin <lin.louis@pm.me>
2025-06-28 22:11:34 +0200

    nvim(ui): change dashboard keys

--- stow/nvim/.config/nvim/lua/plugins/ui.lua
+++ stow/nvim/.config/nvim/lua/plugins/ui.lua
@@ -50,6 +50,19 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = {
+        preset = {
+          keys = {
+            { icon = " ", key = "g", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "f", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
         sections = {
           { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
           { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
