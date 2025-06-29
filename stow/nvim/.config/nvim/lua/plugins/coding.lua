ef91aa5df49977f7d352e25c1c3b2ebd36a4b7f7
Louis Lin <lin.louis@pm.me>
2025-06-29 11:25:25 +0200

    nvim(editor): fix find_sub_module in mini-files

--- stow/nvim/.config/nvim/lua/plugins/coding.lua
+++ stow/nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,29 +1,3 @@
-local function find_sub_module()
-  local relative_filepath = vim.fn.expand("%:.")
-  local _, extension = relative_filepath:match("(.+)%.(.+)$")
-
-  if extension == "rb" then
-    local parts = vim.fn.split(relative_filepath, "/")
-    if #parts > 2 and parts[1] == "engines" then
-      return "engines/" .. parts[2]
-    end
-  end
-
-  if extension == "java" then
-    local filepath = vim.fn.expand("%:.")
-    local dir = vim.fn.fnamemodify(filepath, ":h")
-    while dir ~= "/" do
-      local pom_file = dir .. "/pom.xml"
-      if vim.fn.filereadable(pom_file) == 1 then
-        return vim.fn.fnamemodify(dir, ":t")
-      end
-      dir = vim.fn.fnamemodify(dir, ":h")
-    end
-  end
-
-  return vim.api.nvim_buf_get_name(0)
-end
-
 return {
   -- #######################
   -- override default config
@@ -42,22 +16,6 @@ return {
 
   -- autopairs - not quite as good as nvim-autopairs
   { "echasnovski/mini.pairs", enabled = false },
-
-  -- Navigate and manipulate file system.
-  {
-    "echasnovski/mini.files",
-    optional = true,
-    keys = {
-      {
-        "<leader>fh",
-        function()
-          require("mini.files").open(find_sub_module(), true)
-        end,
-        desc = "Open mini.files in current sub-module/sub-project",
-        remap = true,
-      },
-    },
-  },
 
   -- #######################
   -- add new plugins
--- stow/nvim/.config/nvim/lua/plugins/extras/editor/mini-files.lua
+++ stow/nvim/.config/nvim/lua/plugins/extras/editor/mini-files.lua
@@ -1,3 +1,29 @@
+local function find_sub_module()
+  local relative_filepath = vim.fn.expand("%:.")
+  local _, extension = relative_filepath:match("(.+)%.(.+)$")
+
+  if extension == "rb" then
+    local parts = vim.fn.split(relative_filepath, "/")
+    if #parts > 2 and parts[1] == "engines" then
+      return "engines/" .. parts[2]
+    end
+  end
+
+  if extension == "java" then
+    local filepath = vim.fn.expand("%:.")
+    local dir = vim.fn.fnamemodify(filepath, ":h")
+    while dir ~= "/" do
+      local pom_file = dir .. "/pom.xml"
+      if vim.fn.filereadable(pom_file) == 1 then
+        return dir
+      end
+      dir = vim.fn.fnamemodify(dir, ":h")
+    end
+  end
+
+  return vim.api.nvim_buf_get_name(0)
+end
+
 return {
   -- #######################
   -- override default config
@@ -7,6 +33,14 @@ return {
     "echasnovski/mini.files",
     keys = {
       { "<A-1>", "<leader>fm", desc = "Open mini.files (directory of current file) (Alt+1)", remap = true },
+      {
+        "<leader>fh",
+        function()
+          require("mini.files").open(find_sub_module(), true)
+        end,
+        desc = "Open mini.files in current sub-module/sub-project",
+        remap = true,
+      },
     },
     opts = {
       windows = {
