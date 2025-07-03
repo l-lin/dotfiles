809ae9ba04f45bcf862f7ef10b216cafe4a51941
Louis Lin <lin.louis@pm.me>
2025-07-03 23:16:47 +0200

    nvim(ai): use gpt-4.1 by default

--- stow/nvim/.config/nvim/lua/plugins/custom/ai/codecompanion/adapters.lua
+++ stow/nvim/.config/nvim/lua/plugins/custom/ai/codecompanion/adapters.lua
@@ -4,7 +4,7 @@ return {
     return require("codecompanion.adapters").extend("copilot", {
       name = "copilot_custom",
       schema = {
-        model = { default = "claude-sonnet-4" },
+        model = { default = "gpt-4.1" },
         temperature = { default = 0 },
         max_tokens = { default = 64000 },
       },
