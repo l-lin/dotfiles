3e2712a366ae565de2579c3c4b8aae8f36a262ca
Louis Lin <lin.louis@pm.me>
2025-07-05 11:31:32 +0200

    ai(goose): move config to hm

---
+++ home-manager/modules/share/ai/goose/default.nix
@@ -0,0 +1,8 @@
+#
+# Open-source, extensible AI agent that goes beyond code suggestions - install, execute, edit, and test with any LLM
+# src: https://github.com/block/goose
+#
+
+{
+  xdg.configFile."goose/config.yaml".source = ./.config/goose/config.yaml;
+}
