03c2c6c0e74e520f5a9213a7f03aea759676629b
Louis Lin <lin.louis@pm.me>
2025-07-13 17:47:47 +0200

    ai: add container-use

---
+++ home-manager/modules/share/ai/container-use/default.nix
@@ -0,0 +1,8 @@
+#
+# Sandboxed dev environments for coding agents.
+# src: https://container-use.com/
+#
+
+{
+  xdg.configFile."mise/conf.d/container-use.toml".source = ./.config/mise/conf.d/container-use.toml;
+}
