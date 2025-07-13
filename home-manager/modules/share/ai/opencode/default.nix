73e08361895f2d7d87493f72ce6554ece8774c69
Louis Lin <lin.louis@pm.me>
2025-07-13 18:16:12 +0200

    ai(opencode): update config

--- home-manager/modules/share/ai/opencode/default.nix
+++ home-manager/modules/share/ai/opencode/default.nix
@@ -8,4 +8,5 @@
 {
   xdg.configFile."mise/conf.d/opencode.toml".source = ./.config/mise/conf.d/opencode.toml;
   xdg.configFile."opencode/config.json".source = ./.config/opencode/config.json;
+  xdg.configFile."opencode/AGENTS.md".source = ../.config/ai/conventions/code.md;
 }
