3948e2ee2025b3d56837b3f5c0a7a71636a33a94
Louis Lin <lin.louis@pm.me>
2025-07-19 16:25:41 +0200

    ai: remove container-use

--- home-manager/modules/share/ai/container-use/default.nix
+++
@@ -1,8 +0,0 @@
-#
-# Sandboxed dev environments for coding agents.
-# src: https://container-use.com/
-#
-
-{
-  xdg.configFile."mise/conf.d/container-use.toml".source = ./.config/mise/conf.d/container-use.toml;
-}
