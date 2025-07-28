551f757541efa43b4a22a80cffdc6ca87c0f6ce8
Louis Lin <lin.louis@pm.me>
2025-07-28 20:38:50 +0200

    ai: create sub-agent git-committer

--- stow/nvim/.config/nvim/lua/plugins/custom/ai/prompts/git-committer.lua
+++ stow/nvim/.config/nvim/lua/plugins/custom/ai/prompts/git-committer.lua
@@ -2,7 +2,7 @@ return {
   kind = "action",
   tools = "@{cmd_runner} @{get_changed_files}",
   system = function()
-    local path = os.getenv("XDG_CONFIG_HOME") .. "/ai/prompts/git-commit.md"
+    local path = os.getenv("XDG_CONFIG_HOME") .. "/ai/agents/git-committer.md"
     local file = assert(io.open(path, "r"))
     local content = file:read("*a")
     file:close()
