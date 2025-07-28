f048183074076cc666580c85b3be8b637788d3a4
Louis Lin <lin.louis@pm.me>
2025-07-28 20:51:25 +0200

    ai: create sub-agent researcher-organizer

--- stow/nvim/.config/nvim/lua/plugins/custom/ai/prompts/researcher-organizer.lua
+++ stow/nvim/.config/nvim/lua/plugins/custom/ai/prompts/researcher-organizer.lua
@@ -2,7 +2,7 @@ return {
   kind = "role",
   tools = "@{files} @{web_search} @{fetch_webpage} #{buffer}",
   system = function()
-    local path = os.getenv("XDG_CONFIG_HOME") .. "/ai/prompts/researcher.md"
+    local path = os.getenv("XDG_CONFIG_HOME") .. "/ai/agents/researcher-organizer.md"
     local file = assert(io.open(path, "r"))
     local content = file:read("*a")
     file:close()
