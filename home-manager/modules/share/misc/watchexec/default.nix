0548d0ac9defc68828f78703cd0e3cb1b26190c9
Louis Lin <lin.louis@pm.me>
2025-07-29 20:32:59 +0200

    misc: add watchexec

---
+++ home-manager/modules/share/misc/watchexec/default.nix
@@ -0,0 +1,8 @@
+#
+# Executes commands in response to file modifications
+# src: https://github.com/watchexec/watchexec
+#
+
+{
+  xdg.configFile."mise/conf.d/watchexec.toml".source = ./.config/mise/conf.d/watchexec.toml;
+}
