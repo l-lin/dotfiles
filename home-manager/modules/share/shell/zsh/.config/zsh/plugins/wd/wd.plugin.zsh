756e2af9c517dd1b5ffc52305f7c547ff45a3e62
Louis Lin <lin.louis@pm.me>
2025-08-02 16:00:39 +0200

    zsh(wd): use tmux popup instead

--- home-manager/modules/share/shell/zsh/.config/zsh/plugins/wd/wd.plugin.zsh
+++ home-manager/modules/share/shell/zsh/.config/zsh/plugins/wd/wd.plugin.zsh
@@ -8,51 +8,22 @@ if ! type wd fzf >/dev/null 2>&1; then
   return
 fi
 
-# Shamelessly copied and adapted from original.
-# src: https://github.com/mfaerevaag/wd/blob/f0f47b7197f24eeb34f5481e1e01e41becb61174/wd.sh#L255-L286
-custom_wd_browse() {
-    if ! command -v fzf >/dev/null; then
-        echo "This functionality requires fzf. Please install fzf first."
-        return 1
-    fi
-    local entries=("${(@f)$(sed "s:${HOME}:~:g" "$WD_CONFIG")}")
-    local script_path=$(antidote path mfaerevaag/wd)
-    local wd_remove_output=$(mktemp "${TMPDIR:-/tmp}/wd.XXXXXXXXXX")
-    local fzf_bind="delete:execute(echo {} | awk '{print \$1}' | xargs -I {} "$script_path/wd.sh" rm {} > "$wd_remove_output")+abort"
-    # Customize the fzf option.
-    local selected_entry=$( \
-      printf '%s\n' "${entries[@]}" \
-      | column -s':' -t \
-      | fzf \
-        --header='Enter: select | Delete: remove' \
-        --height 30% \
-        --no-reverse \
-        --bind="$fzf_bind" \
-      | awk '{ print $1 }'
-    )
-    if [[ -e $wd_remove_output ]]; then
-        cat "$wd_remove_output"
-        rm "$wd_remove_output"
-    fi
-    if [[ -n $selected_entry ]]; then
-        local selected_point="${selected_entry%% ->*}"
-        selected_point=$(echo "$selected_point" | xargs)
-        wd $selected_point
-    fi
-}
+_wd_browse() {
+  local entries=("${(@f)$(sed "s:${HOME}:~:g" "${WD_CONFIG:-${HOME}/.warprc}")}")
+  local cmd="printf '%s\n' "${entries[@]}" | column -s':' -t | fzf --no-reverse --no-header"
 
-custom_wd_browse_widget() {
-  if [[ -e $WD_CONFIG ]]; then
-    custom_wd_browse
-    saved_buffer=$BUFFER
-    saved_cursor=$CURSOR
-    BUFFER=
-    zle redisplay
-    zle accept-line
+  if [ -n "${TMUX_PANE-}" ]; then
+    # Not the best result, as it will display the command sent to the tmux pane, but it does the job.
+    tmux popup -x C -y 0 -w 50% -h 50% -E "result=\$(${cmd} | awk '{ print \$1 }') && tmux send-keys \"wd \$result\" Enter"
   fi
 }
 
-zle -N custom_wd_browse_widget
+_wd_browse_widget() {
+  LBUFFER="${LBUFFER}$(_wd_browse)"
+  local ret=$?
+  zle reset-prompt
+  return $ret
+}
 
-bindkey ${FZF_WD_BINDKEY:-'^B'} custom_wd_browse_widget
-
+zle -N _wd_browse_widget
+bindkey '^B' _wd_browse_widget
