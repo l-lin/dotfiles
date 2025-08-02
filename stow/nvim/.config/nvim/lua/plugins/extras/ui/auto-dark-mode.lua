602b3bfc92107fc6fd434e88979d20f6868d4781
Louis Lin <lin.louis@pm.me>
2025-08-02 14:11:03 +0200

    nvim(ui): call github_light_high_contrast twice too

--- stow/nvim/.config/nvim/lua/plugins/extras/ui/auto-dark-mode.lua
+++ stow/nvim/.config/nvim/lua/plugins/extras/ui/auto-dark-mode.lua
@@ -3,13 +3,14 @@ return {
     "f-person/auto-dark-mode.nvim",
     lazy = false,
     opts = {
+      -- For some reason, I need to call it twice to have all the right
+      -- colors, like in markdown or the TODO comments.
       set_dark_mode = function()
-        -- For some reason, I need to call it twice to have all the right
-        -- colors, like in markdown or the TODO comments.
         vim.cmd("colorscheme kanagawa")
         vim.cmd("colorscheme kanagawa")
       end,
       set_light_mode = function()
+        vim.cmd("colorscheme github_light_high_contrast")
         vim.cmd("colorscheme github_light_high_contrast")
       end,
       update_interval = 3000,
