81386c1c898ffa4d4901fd950ee2e98914bbd701
Louis Lin <lin.louis@pm.me>
2025-08-04 21:32:43 +0200

    vcs: remove forgit
    Useless, I was only using `gcb` but even that, I could use `git switch <tab>`.

--- home-manager/modules/share/vcs/forgit/default.nix
+++ home-manager/modules/share/vcs/forgit/default.nix
@@ -3,15 +3,16 @@
 # src: https://github.com/wfxr/forgit
 #
 
-{
-  home.sessionVariables = {
-    FORGIT_COPY_CMD = "$COPY_TO_CLIPBOARD";
-    FORGIT_FZF_DEFAULT_OPTS = "--no-reverse --header '?: toogle preview | C-y: yank commit hash | (S-)Tab: mark | C-r: toggle selection | A-(k/j): move preview up/down | A-e: edit file'";
-  };
-
-  # Symlink ~/.config/zsh/plugins/forgit
-  xdg.configFile."zsh/plugins/forgit" = {
-    source = ./.config/zsh/plugins/forgit;
-    recursive = true;
-  };
-}
+{}
+# {
+#   home.sessionVariables = {
+#     FORGIT_COPY_CMD = "$COPY_TO_CLIPBOARD";
+#     FORGIT_FZF_DEFAULT_OPTS = "--no-reverse --header '?: toogle preview | C-y: yank commit hash | (S-)Tab: mark | C-r: toggle selection | A-(k/j): move preview up/down | A-e: edit file'";
+#   };
+#
+#   # Symlink ~/.config/zsh/plugins/forgit
+#   xdg.configFile."zsh/plugins/forgit" = {
+#     source = ./.config/zsh/plugins/forgit;
+#     recursive = true;
+#   };
+# }
