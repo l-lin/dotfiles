49015ed5241d664d2de48e3bd571d4804f87896b
Louis Lin <lin.louis@pm.me>
2025-08-05 20:56:13 +0200

    nvim(keymaps): do not search previous element

--- stow/nvim/.config/nvim/lua/config/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/keymaps.lua
@@ -123,3 +123,4 @@ vim.keymap.del("n", "<c-_>")
 
 -- Do not select the next search element, so that I can easily do `cgn`.
 map("n", "*", "*N", { noremap = true, silent = true })
+map("n", "#", "#N", { noremap = true, silent = true })
