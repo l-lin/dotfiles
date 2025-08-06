698b5ee3e93fc121bef65efa387b0363d84b8477
Louis Lin <lin.louis@pm.me>
2025-08-06 12:14:59 +0200

    nvim(kotlin): reduce maven logs + better feedback

--- stow/nvim/.config/nvim/lua/plugins/custom/lang/kotlin.lua
+++ stow/nvim/.config/nvim/lua/plugins/custom/lang/kotlin.lua
@@ -5,14 +5,18 @@ local function execute_all_tests_with_maven()
   local filename = vim.fn.expand('%:t')
   if string.match(filename, "%.kt$") then
     local tests = string.gsub(filename, "%.kt$", "")
-    local command_to_run = "mvn test -Dtest=" .. tests
+    local command_to_run = "./mvnw test -q -Dtest=" .. tests
+
+    local success_log = 'echo -e \\"\\e[1;30;42m SUCCESS \\e[0m\\"'
 
     -- `-l 20` specifies the size of the tmux pane, in this case 20 rows
     vim.cmd(
       "silent !tmux split-window -v -l 20 '"
-        .. 'zsh -i -c "'
-        .. command_to_run
-        .. "; echo; echo Press q to exit...; while true; do read -k 1 key; if [[ \\$key == \"q\" ]]; then exit; fi; done\"'"
+        .. 'bash -i -c "'
+        .. 'echo \\"' .. command_to_run .. '\\"'
+        .. ' && ' .. command_to_run
+        .. ' && ' .. success_log
+        .. "; echo; echo Press q to exit...; while true; do read -n 1 key; if [[ \\$key == \"q\" ]]; then exit; fi; done\"'"
     )
   else
     vim.notify("Not a Kotlin file.", vim.log.levels.WARN)
