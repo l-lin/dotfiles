747cc374eeee1ce12af109cfb8350c9b6cd043a7
Louis Lin <lin.louis@pm.me>
2025-08-11 20:40:07 +0200

    nvim: remove harpoon

--- stow/nvim/.config/nvim/lua/config/lazy.lua
+++ stow/nvim/.config/nvim/lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ai.codeium" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    -- { import = "lazyvim.plugins.extras.editor.harpoon2" },
     -- { import = "lazyvim.plugins.extras.editor.mini-diff" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     -- { import = "lazyvim.plugins.extras.editor.overseer" },
