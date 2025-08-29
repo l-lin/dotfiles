9820f9939a5f7f26000c071e182cf33d218b08c0
Louis Lin <lin.louis@pm.me>
2025-08-29 22:14:18 +0200

    misc: add gonzo

---
+++ home-manager/modules/share/misc/gonzo/default.nix
@@ -0,0 +1,10 @@
+#
+# A powerful, real-time log analysis terminal UI inspired by k9s. Analyze log
+# streams with beautiful charts, AI-powered insights, and advanced filtering -
+# all from your terminal.
+# src: https://gonzo.controltheory.com/
+#
+
+{
+  xdg.configFile."mise/conf.d/gonzo.toml".source = ./.config/mise/conf.d/gonzo.toml;
+}
