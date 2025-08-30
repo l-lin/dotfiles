1a59796a9a2524ae004af395a7782fbdcc707798
Louis Lin <lin.louis@pm.me>
2025-08-30 12:38:48 +0200

    json: add fx

--- home-manager/modules/share/misc/json/default.nix
+++ home-manager/modules/share/misc/json/default.nix
@@ -6,6 +6,8 @@
   home.packages = with pkgs; [
     # Make JSON greppable: https://github.com/tomnomnom/gron
     gron
+    # Terminal JSON viewer & processor: https://fx.wtf/
+    fx
     # Interactive jq: https://git.sr.ht/~gpanders/ijq
     ijq
     # Pager for JSON data: https://jless.io/
