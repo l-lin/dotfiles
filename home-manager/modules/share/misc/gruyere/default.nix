bd06af3487bafdf14b645844cb8924d7b737d6e5
Louis Lin <lin.louis@pm.me>
2025-09-20 11:32:25 +0200

    misc: add gruyere to find program using some ports

---
+++ home-manager/modules/share/misc/gruyere/default.nix
@@ -0,0 +1,8 @@
+#
+# A tiny (and pretty) program for viewing + killing listening ports.
+# src: https://github.com/savannahostrowski/gruyere
+#
+
+{
+  xdg.configFile."mise/conf.d/gruyere.toml".source = ./.config/mise/conf.d/gruyere.toml;
+}
