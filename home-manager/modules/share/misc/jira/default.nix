cf3cecff433c249c3c8a80ad16e6d4d3d6a549b0
Louis Lin <lin.louis@pm.me>
2025-09-24 16:52:03 +0200

    jira: add open-jira-ticket

--- home-manager/modules/share/misc/jira/default.nix
+++ home-manager/modules/share/misc/jira/default.nix
@@ -8,4 +8,5 @@
 
   # Symlink ~/.config/zsh/*
   xdg.configFile."zsh/completions/_jira".source = ./.config/zsh/completions/_jira;
+  xdg.configFile."zsh/functions/open-jira-ticket".source = ./.config/zsh/functions/open-jira-ticket;
 }
