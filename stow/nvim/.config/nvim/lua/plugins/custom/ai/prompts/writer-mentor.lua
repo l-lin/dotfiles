4ee633c6627247b9c823373e3b0fd18bbd4fc313
Louis Lin <lin.louis@pm.me>
2025-09-29 19:08:51 +0200

    ai: add writer-mentor

---
+++ stow/nvim/.config/nvim/lua/plugins/custom/ai/prompts/writer-mentor.lua
@@ -0,0 +1,14 @@
+return {
+  kind = "role",
+  tools = "",
+  system = function()
+    local path = os.getenv("XDG_CONFIG_HOME") .. "/ai/prompts/writer-mentor.md"
+    local file = assert(io.open(path, "r"))
+    local content = file:read("*a")
+    file:close()
+    return content
+  end,
+  user = function()
+    return ""
+  end,
+}
