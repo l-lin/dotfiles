a11da9cb6a9c7ce0c5e79bc3be841600667e7f0c
Louis Lin <lin.louis@pm.me>
2025-10-19 18:06:55 +0200

    flake: update

--- home-manager/modules/share/misc/tealdeer/default.nix
+++ home-manager/modules/share/misc/tealdeer/default.nix
@@ -3,9 +3,10 @@
 # src: https://github.com/dbrgn/tealdeer
 #
 
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ tealdeer ];
-
-  # Symlink to ~/.config/tealdeer/config.toml
-  xdg.configFile."tealdeer/config.toml".source = ./.config/tealdeer/config.toml;
-}
+{}
+# { pkgs, ... }: {
+#   home.packages = with pkgs; [ tealdeer ];
+#
+#   # Symlink to ~/.config/tealdeer/config.toml
+#   xdg.configFile."tealdeer/config.toml".source = ./.config/tealdeer/config.toml;
+# }
