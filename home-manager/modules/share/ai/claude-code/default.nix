d29dea569c1b1803016ce1870e08eb4b9ff939c5
Louis Lin <lin.louis@pm.me>
2025-10-21 11:52:03 +0200

    ai(claude-code): move mcp to their own directory

--- home-manager/modules/share/ai/claude-code/default.nix
+++ home-manager/modules/share/ai/claude-code/default.nix
@@ -9,9 +9,10 @@
 
   home.file.".claude/settings.json".source = ./.claude/settings.json;
   home.file.".claude/cc_statusline.rb".source = ./.claude/cc_statusline.rb;
-  home.file.".claude/mcp-atlassian.json".source = ./.claude/mcp-atlassian.json;
-  home.file.".claude/mcp-datadog.json".source = ./.claude/mcp-datadog.json;
-  home.file.".claude/mcp-sequentialthinking.json".source = ./.claude/mcp-sequentialthinking.json;
+  home.file.".claude/mcp" = {
+    source = ./.claude/mcp;
+    recursive = true;
+  };
   home.file.".claude/commands" = {
     source = ../.config/ai/prompts;
     recursive = true;
