b3f15a285a0acc33145d2a12859a36af0916efd1
Louis Lin <lin.louis@pm.me>
2025-11-16 16:19:34 +0100

    style: add nord colorscheme

---
+++ home-manager/modules/share/style/themes/nord/default.nix
@@ -0,0 +1,35 @@
+#
+# Nord Theme - Polar Night, Snow Storm, Frost, Aurora
+# src: https://www.nordtheme.com/ and https://github.com/gbprod/nord.nvim
+#
+
+{ config, pkgs, ... }:
+let
+  polarity = "dark";
+in {
+  theme = {
+    inherit polarity;
+    nvimColorScheme = "nord";
+    nvimColorSchemePluginLua = ''
+{
+  "gbprod/nord.nvim",
+  opts = {
+    errors = "fg",
+    styles = {
+      keywords = { bold = true },
+    },
+  },
+}
+'';
+  };
+
+  stylix = {
+    inherit polarity;
+    image = "${config.home.homeDirectory}/Pictures/${polarity}.jpg";
+    cursor = {
+      name = "Bibata-Modern-Ice";
+      size = 24;
+      package = pkgs.bibata-cursors;
+    };
+  };
+}
