1c886e92116970250a7b3ce17dcd66b1dad89693
Louis Lin <lin.louis@pm.me>
2026-01-11 09:31:04 +0100

    wm(aerospace): scratch neovim that yank the content on exit

--- home-manager/modules/share/editor/nvim/.config/nvim/lua/plugins/persistence.lua
+++ home-manager/modules/share/editor/nvim/.config/nvim/lua/plugins/persistence.lua
@@ -2,6 +2,9 @@ return {
   "folke/persistence.nvim",
   lazy = false,
   opts = function(_, opts)
+    -- Do not auto-restore session for scratch instances
+    if vim.env.NVIM_SCRATCH == "1" then return opts end
+
     -- Auto-restore session when opening nvim without arguments
     vim.api.nvim_create_autocmd("VimEnter", {
       group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
