58a7feda7ca565db2b4cff4f00fe64401dd92fa1
Louis Lin <lin.louis@pm.me>
2026-01-15 14:00:03 +0100

    nvim(ruby): add private-ruby.nvim and disable fuzzy_ruby_server

--- home-manager/modules/share/editor/nvim/.config/nvim/lua/plugins/extras/lang/ruby.lua
+++ home-manager/modules/share/editor/nvim/.config/nvim/lua/plugins/extras/lang/ruby.lua
@@ -16,6 +16,11 @@ return {
       },
     },
   },
+  {
+    "l-lin/private-ruby.nvim",
+    ft = 'ruby',
+    opts = {},
+  },
 
   -- add syntax highlighting
   {
@@ -46,29 +51,30 @@ return {
   },
 
   -- Alternative LSP server to ruby-lsp for navigation (no auto-completion).
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "pheen/fuzzy_ruby_server" },
-    opts = {
-      servers = {
-        fuzzy_ls = {
-          enabled = true,
-          filetypes = { "ruby" },
-          root_markers = { 'Gemfile', '.git' },
-          mason = false,
-          init_options = {
-            -- possible values:
-            -- ram: use RAM (can be very high on big project)
-            -- tempdir: use mmap directory to store the indexes (e.g. /tmp/.tmpcCUkiK)
-            allocationType = "ram",
-            indexGems = true,
-            reportDiagnostics = true,
-          },
-          cmd = {
-            vim.fn.expand(vim.fn.stdpath("data") .. "/lazy/fuzzy_ruby_server/bin/fuzzy_darwin-arm64"),
-          },
-        },
-      },
-    },
-  },
+  -- NOTE: disabling it because it's really resource hungry (RAM+CPU) for large projects
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   dependencies = { "pheen/fuzzy_ruby_server" },
+  --   opts = {
+  --     servers = {
+  --       fuzzy_ls = {
+  --         enabled = true,
+  --         filetypes = { "ruby" },
+  --         root_markers = { 'Gemfile', '.git' },
+  --         mason = false,
+  --         init_options = {
+  --           -- possible values:
+  --           -- ram: use RAM (can be very high on big project)
+  --           -- tempdir: use mmap directory to store the indexes (e.g. /tmp/.tmpcCUkiK)
+  --           allocationType = "ram",
+  --           indexGems = true,
+  --           reportDiagnostics = true,
+  --         },
+  --         cmd = {
+  --           vim.fn.expand(vim.fn.stdpath("data") .. "/lazy/fuzzy_ruby_server/bin/fuzzy_darwin-arm64"),
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
