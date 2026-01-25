054e5d04ced5554b48b23f865ef6778fc0a6b20a
Louis Lin <lin.louis@pm.me>
2026-01-25 14:23:42 +0100

    video: disable mpv
    Its build was too much... 30 minutes and still not done. Moreover, it was
    rebuilding with swift and causing 90% CPU usage... WTH.

--- home-manager/modules/share/video/default.nix
+++ home-manager/modules/share/video/default.nix
@@ -7,6 +7,6 @@
     # A complete, cross-platform solution to record, convert and stream audio and video: https://www.ffmpeg.org/
     ffmpeg
     # General-purpose media player, fork of MPlayer and mplayer2: https://mpv.io/
-    mpv
+    # mpv
   ];
 }
