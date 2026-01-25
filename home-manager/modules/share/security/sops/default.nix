7c71e8af239cba1fd869d4e1ea8d84fe60b338ca
Louis Lin <lin.louis@pm.me>
2026-01-25 15:55:20 +0100

    security(sops): fix for macOS

--- home-manager/modules/share/security/sops/default.nix
+++ home-manager/modules/share/security/sops/default.nix
@@ -5,8 +5,9 @@
 # - https://github.com/Mic92/sops-nix
 #
 
-{ config, inputs, pkgs, systemSettings, ... }: 
+{ config, inputs, lib, pkgs, ... }:
 let
+  inherit (pkgs.stdenv) isDarwin;
   # When decrypting a file with the corresponding identity, SOPS will look for a text file name keys.txt
   # located in a sops subdirectory of your user configuration directory.
   # On Linux, this would be $XDG_CONFIG_HOME/sops/age/keys.
@@ -14,7 +15,7 @@ let
   # On macOS, this would be $HOME/Library/Application Support/sops/age/keys.txt.
   #
   # src: https://github.com/getsops/sops?tab=readme-ov-file#encrypting-using-age
-  sopsKeyFile = if (systemSettings.system == "aarch64-darwin") then
+  sopsKeyFile = if isDarwin then
     "${config.home.homeDirectory}/Library/Application\ Support/sops/age/keys.txt"
   else
     "${config.xdg.configHome}/sops/age/keys.txt";
@@ -39,5 +40,13 @@ in {
     #     "ssh-to-age -private-key -i ~/.ssh/l-lin >> ~/.config/sops/age/keys.txt"
     # ```
     keyFile = sopsKeyFile;
+  };
+
+  # Workaround for sops-nix LaunchAgent empty PATH on macOS.
+  # The LaunchAgent's PATH is empty when no age plugins are configured,
+  # causing getconf to not be found.
+  # src: https://github.com/Mic92/sops-nix/issues/890
+  launchd.agents.sops-nix = lib.mkIf isDarwin {
+    config.EnvironmentVariables.PATH = lib.mkForce "/usr/bin:/bin:/usr/sbin:/sbin";
   };
 }
