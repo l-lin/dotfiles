b8c18e018d36963068fd51b0ac7485136629b16f
Louis Lin <lin.louis@pm.me>
2026-02-07 10:13:38 +0100

    hm-ricing-mode: add ai & zsh

--- home-manager/modules/share/hm-ricing-mode/default.nix
+++ home-manager/modules/share/hm-ricing-mode/default.nix
@@ -7,10 +7,20 @@
   programs.hm-ricing-mode = {
     enable = true;
     apps = {
+      ai = {
+        dest_dir = ".config/ai";
+        source_dir = "${symlinkRoot}/home-manager/modules/share/ai/.config/ai";
+        # symlink | backport (default)
+        type = "symlink";
+      };
       pi = {
         dest_dir = ".pi/agent/extensions";
         source_dir = "${symlinkRoot}/home-manager/modules/share/ai/pi/.pi/agent/extensions";
-        # symlink | backport (default)
+        type = "symlink";
+      };
+      zsh = {
+        dest_dir = ".config/zsh";
+        source_dir = "${symlinkRoot}home-manager/modules/share/shell/zsh/.config/zsh";
         type = "symlink";
       };
     };
