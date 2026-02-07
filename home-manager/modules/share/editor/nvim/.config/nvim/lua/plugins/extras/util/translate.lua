b99236c5abdcb4f0405caa6921caeeed0cc79b1c
Louis Lin <lin.louis@pm.me>
2026-02-07 09:59:54 +0100

    nvim: update keymaps

--- home-manager/modules/share/editor/nvim/.config/nvim/lua/plugins/extras/util/translate.lua
+++ home-manager/modules/share/editor/nvim/.config/nvim/lua/plugins/extras/util/translate.lua
@@ -6,9 +6,9 @@ return {
     cmd = "Translate",
     opts = { default_lang = "en" },
     keys = {
-      { "<leader>od", function() require("translate").replace_selection() end, mode = "v", desc = "Translate & replace" },
-      { "<leader>od", function() require("translate").replace_cword() end, mode = "n", desc = "Translate & replace word under cursor" },
-      { "<leader>oe", function() require("translate").translate(selector.get_selected_text()) end, mode = "v", desc = "Translate selected text" },
+      { "<leader>oD", function() require("translate").replace_selection() end, mode = "v", desc = "Translate & replace" },
+      { "<leader>oD", function() require("translate").replace_cword() end, mode = "n", desc = "Translate & replace word under cursor" },
+      { "<leader>od", function() require("translate").translate(selector.get_selected_text()) end, mode = "v", desc = "Translate selected text" },
     },
   }
 }
