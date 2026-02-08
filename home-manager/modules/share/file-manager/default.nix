e2122841e05e0dcb9d0de8b8b8fa88547e58c506
Louis Lin <lin.louis@pm.me>
2026-02-08 10:43:52 +0100

    file: add jocalsend for offline airdrop

--- home-manager/modules/share/file-manager/default.nix
+++ home-manager/modules/share/file-manager/default.nix
@@ -2,7 +2,12 @@
 # File manager.
 #
 
-{ userSettings, ... }: {
+{ pkgs, userSettings, ... }: {
+  home.packages = with pkgs; [
+    # Rust terminal client for Localsend: https://git.kittencollective.com/nebkor/joecalsend
+    jocalsend
+  ];
+
   imports = [
     (./. + "/${userSettings.fileManager}")
   ];
