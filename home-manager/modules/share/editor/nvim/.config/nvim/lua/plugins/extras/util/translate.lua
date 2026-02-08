492b0cfb1faedd74bfd9075efbdb28a7191adcaa
Louis Lin <lin.louis@pm.me>
2026-02-08 11:38:02 +0100

    nvim(translate): add translation for word under cursor

--- home-manager/modules/share/editor/nvim/.config/nvim/lua/plugins/extras/util/translate.lua
+++ home-manager/modules/share/editor/nvim/.config/nvim/lua/plugins/extras/util/translate.lua
@@ -8,6 +8,7 @@ return {
     keys = {
       { "<leader>oD", function() require("translate").replace_selection() end, mode = "v", desc = "Translate & replace" },
       { "<leader>oD", function() require("translate").replace_cword() end, mode = "n", desc = "Translate & replace word under cursor" },
+      { "<leader>od", function() require("translate").translate(vim.fn.expand("<cword>")) end, mode = "n", desc = "Translate word under cursor" },
       { "<leader>od", function() require("translate").translate(selector.get_selected_text()) end, mode = "v", desc = "Translate selected text" },
     },
   }
