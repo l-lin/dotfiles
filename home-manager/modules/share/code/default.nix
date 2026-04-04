05e64d2ad958f7d9a315a82ca49dcd5b2a7bfbed
Louis Lin <lin.louis@pm.me>
2026-04-04 21:54:33 +0200

    code: add tree-sitter

--- home-manager/modules/share/code/default.nix
+++ home-manager/modules/share/code/default.nix
@@ -8,6 +8,9 @@
   home.packages = with pkgs; [
     # Maintained ctags implementation: https://docs.ctags.io/en/latest/
     universal-ctags
+
+    # Parser generator tool and an incremental parsing library: https://github.com/tree-sitter/tree-sitter
+    tree-sitter
   # HACK: DISABLED because work internal tool needs native gcc and make.
   #   # GNU Compiler Collection.
   #   gcc
