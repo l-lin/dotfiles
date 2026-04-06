1e8fd3f3ca2b5939016ce9b5f31e9a9a7f0f8699
Louis Lin <lin.louis@pm.me>
2026-04-06 19:45:06 +0200

    nvim(editor): remove emoji

--- home-manager/modules/share/editor/nvim/.config/nvim-pack/lua/plugins/editor/init.lua
+++ home-manager/modules/share/editor/nvim/.config/nvim-pack/lua/plugins/editor/init.lua
@@ -1,5 +1,5 @@
 return {
-  require("plugins.editor.emoji"),
+  -- require("plugins.editor.emoji"),
   require("plugins.editor.global-note"),
   require("plugins.editor.harpoon"),
   require("plugins.editor.mini-files"),
