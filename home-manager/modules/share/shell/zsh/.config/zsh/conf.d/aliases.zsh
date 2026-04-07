bda130b285af7d326c60b0270c4b8cfa1cf9f056
Louis Lin <lin.louis@pm.me>
2026-04-07 08:50:22 +0200

    zsh: change alias for nvim-lazy

--- home-manager/modules/share/shell/zsh/.config/zsh/conf.d/aliases.zsh
+++ home-manager/modules/share/shell/zsh/.config/zsh/conf.d/aliases.zsh
@@ -26,7 +26,7 @@ alias n="nix"
 alias ns="nix-shell --command zsh -p"
 # editor
 alias v="nvim"
-alias np="NVIM_APPNAME=nvim-pack nvim"
+alias lv="NVIM_APPNAME=nvim-lazy nvim"
 # sudo
 alias sudo="sudo"
 alias _="sudo"
