e0d71c79ca550c9d1205d450ff0ac022003d8f97
Louis Lin <lin.louis@pm.me>
2026-04-22 20:18:49 +0200

    ai: add openspec

--- home-manager/modules/share/ai/default.nix
+++ home-manager/modules/share/ai/default.nix
@@ -8,6 +8,7 @@
     ./dust
     ./ollama
     #./opencode
+    ./openspec
     ./pi
   ];
 
---
+++ home-manager/modules/share/ai/openspec/default.nix
@@ -0,0 +1,8 @@
+#
+# A lightweight spec-driven framework.
+# src: https://openspec.dev/
+#
+
+{
+  xdg.configFile."mise/conf.d/openspec.toml".source = ./.config/mise/conf.d/openspec.toml;
+}
