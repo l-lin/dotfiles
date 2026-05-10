52415b64a49c15e57564900107a02c6ed59b8d51
Louis Lin <lin.louis@pm.me>
2026-05-10 15:19:56 +0200

    vcs(lazygh): add install from mise

--- home-manager/modules/share/vcs/lazygh/default.nix
+++ home-manager/modules/share/vcs/lazygh/default.nix
@@ -5,4 +5,5 @@
 
 { config, symlinkRoot, ... }: {
   xdg.configFile."lazygh/config.toml".source = config.lib.file.mkOutOfStoreSymlink "${symlinkRoot}/home-manager/modules/share/vcs/lazygh/.config/lazygh/config.toml";
+  xdg.configFile."mise/conf.d/lazygh.toml".source = ./.config/mise/conf.d/lazygh.toml;
 }
