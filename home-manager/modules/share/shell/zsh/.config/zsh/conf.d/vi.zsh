c576093a63376c34ecf197fdaabc01071756b44d
Louis Lin <lin.louis@pm.me>
2026-07-05 16:21:56 +0200

    zsh: move to emacs-mode

--- home-manager/modules/share/shell/zsh/.config/zsh/conf.d/vi.zsh
+++ home-manager/modules/share/shell/zsh/.config/zsh/conf.d/vi.zsh
@@ -3,15 +3,22 @@
 # vi mode
 #
 
-# ensure other plugins keybinding are preserved when using zsh-vi-mode
-# see https://github.com/jeffreytse/zsh-vi-mode/issues/242
-function zvm_after_init() {
-  zvm_bindkey viins '^f' _navi_widget
-}
+typeset _zephyr_editor_key_bindings
+zstyle -s ':zephyr:plugin:editor' key-bindings _zephyr_editor_key_bindings
 
-# delete word with ctrl+w
-# src https://unix.stackexchange.com/a/392199
-autoload -U select-word-style
-select-word-style bash
-export WORDCHARS='.-'
+if [[ "${_zephyr_editor_key_bindings:-emacs}" == 'vi' ]]; then
+  # ensure other plugins keybinding are preserved when using zsh-vi-mode
+  # see https://github.com/jeffreytse/zsh-vi-mode/issues/242
+  function zvm_after_init() {
+    zvm_bindkey viins '^f' _navi_widget
+  }
+
+  # delete word with ctrl+w
+  # src https://unix.stackexchange.com/a/392199
+  autoload -U select-word-style
+  select-word-style bash
+  export WORDCHARS='.-'
+fi
+
+unset _zephyr_editor_key_bindings
 
