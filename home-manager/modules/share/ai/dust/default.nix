089ad6c1e1b3455b8f9f4a1219a52b6245080fd8
Louis Lin <lin.louis@pm.me>
2026-09-06 13:57:26 +0200

    dust: remove dust-cli

--- home-manager/modules/share/ai/dust/default.nix
+++ home-manager/modules/share/ai/dust/default.nix
@@ -3,26 +3,27 @@
 # src: https://dust.tt/
 #
 
-{ pkgs, ... }: {
-  xdg.configFile."mise/conf.d/dust-cli.toml".source = ./.config/mise/conf.d/dust-cli.toml;
-
-  home.packages = with pkgs; [
-    (writeShellScriptBin "dust-cli" ''
-#!/usr/bin/env bash
+{}
+# { pkgs, ... }: {
+#   xdg.configFile."mise/conf.d/dust-cli.toml".source = ./.config/mise/conf.d/dust-cli.toml;
 #
-# Wrapper script to launch opencode-ai, as its name conflicts with the other opencode name.
+#   home.packages = with pkgs; [
+#     (writeShellScriptBin "dust-cli" ''
+# #!/usr/bin/env bash
+# #
+# # Wrapper script to launch opencode-ai, as its name conflicts with the other opencode name.
+# #
 #
-
-set -euo pipefail
-
-BIN_PATH="$(mise bin-paths | grep dust)/dust"
-
-if [ ! -x "$BIN_PATH" ]; then
-  echo "[dust] ERROR: Binary not found at $BIN_PATH" >&2
-  exit 1
-fi
-
-"$BIN_PATH" "$@"
-'')
-  ];
-}
+# set -euo pipefail
+#
+# BIN_PATH="$(mise bin-paths | grep dust)/dust"
+#
+# if [ ! -x "$BIN_PATH" ]; then
+#   echo "[dust] ERROR: Binary not found at $BIN_PATH" >&2
+#   exit 1
+# fi
+#
+# "$BIN_PATH" "$@"
+# '')
+#   ];
+# }
